31d7c20779e507c4f1510be39dfa833fa14a43c6
Stanislav Venzerul <stanislav.venzerul@gmail.com>
2014-04-16 10:26:00 -0400

    adjusting app routes and formatting

--- app/js/app_routes.js
+++ app/js/app_routes.js
@@ -2,19 +2,47 @@
 
 define(['app', 'extended-route-provider'], function(app) {
 
-    app.config(['extendedRouteProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-        
-        $locationProvider.html5Mode(true);
-        $locationProvider.hashPrefix('!');
+  app.config(['extendedRouteProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
 
-        $routeProvider.
-            when('/',                   { templateUrl: '/app/views/index.html',                 resolveController: true, resolveUser: true }).
-            when('/about',              { templateUrl: '/app/views/about.html',                 resolveUser: true }).
-            when('/cms/management/papersmart.shtml',          { templateUrl: '/app/views/samples/country-index.html', resolveController: true, resolveUser: true }).
-            when('/cms/management/papersmart.shtml/:country', { templateUrl: '/app/views/samples/country.html',       resolveController: true, resolveUser: true }).
-            when('/case-studies/:id',   { templateUrl: '/app/views/samples/case-study.html',    resolveController: true, resolveUser: true }).
-            when('/404',                { templateUrl: '/app/views/404.html',                   resolveUser: true }).
-            otherwise({ redirectTo: '/404' });
-    }]);
+      var prefix = '/reports';
+
+      $locationProvider.html5Mode(true);
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider
+        .when(prefix + '/', {
+          templateUrl: '/app/views/index.html',
+          resolveController: true,
+          resolveUser: true
+        })
+        .when('/about', {
+          templateUrl: '/app/views/about.html',
+          resolveUser: true
+        })
+        .when('/cms/management/papersmart.shtml', {
+          templateUrl: '/app/views/samples/country-index.html',
+          resolveController: true,
+          resolveUser: true
+        })
+        .when('/cms/management/papersmart.shtml/:country', {
+          templateUrl: '/app/views/samples/country.html',
+          resolveController: true,
+          resolveUser: true
+        })
+        .when('/case-studies/:id', {
+          templateUrl: '/app/views/samples/case-study.html',
+          resolveController: true,
+          resolveUser: true
+        })
+        .when('/404', {
+          templateUrl: '/app/views/404.html',
+          resolveUser: true
+        })
+        .otherwise({
+          redirectTo: '/404'
+        });
+    }
+  ]);
 
 });
