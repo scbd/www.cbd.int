603800e92b3e133218f469fd074f0427fe66fe90
Stanislav Venzerul <stanislav.venzerul@gmail.com>
2014-04-17 15:08:03 -0400

    reformat according to rules

--- app/js/app_routes.js
+++ app/js/app_routes.js
@@ -5,43 +5,17 @@ define(['app', 'extended-route-provider'], function(app) {
   app.config(['extendedRouteProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
 
-      var prefix = '/reports';
-
       $locationProvider.html5Mode(true);
       $locationProvider.hashPrefix('!');
 
       $routeProvider
-        .when(prefix + '/', {
-          templateUrl: '/app/views/index.html',
-          resolveController: true,
-          resolveUser: true
-        })
-        .when('/about', {
-          templateUrl: '/app/views/about.html',
-          resolveUser: true
-        })
-        .when('/cms/management/papersmart.shtml', {
-          templateUrl: '/app/views/samples/country-index.html',
-          resolveController: true,
-          resolveUser: true
-        })
-        .when('/cms/management/papersmart.shtml/:country', {
-          templateUrl: '/app/views/samples/country.html',
-          resolveController: true,
-          resolveUser: true
-        })
-        .when('/case-studies/:id', {
-          templateUrl: '/app/views/samples/case-study.html',
-          resolveController: true,
-          resolveUser: true
-        })
-        .when('/404', {
-          templateUrl: '/app/views/404.html',
-          resolveUser: true
-        })
-        .otherwise({
-          redirectTo: '/404'
-        });
+        .when('/', {templateUrl: '/app/views/index.html', resolveController: true, resolveUser: true })
+        .when('/about', {templateUrl: '/app/views/about.html', resolveUser: true })
+        .when('/cms/management/papersmart.shtml', {templateUrl: '/app/views/samples/country-index.html', resolveController: true, resolveUser: true })
+        .when('/cms/management/papersmart.shtml/:country', {templateUrl: '/app/views/samples/country.html', resolveController: true, resolveUser: true })
+        .when('/case-studies/:id', {templateUrl: '/app/views/samples/case-study.html', resolveController: true, resolveUser: true })
+        .when('/404', {templateUrl: '/app/views/404.html', resolveUser: true })
+        .otherwise({redirectTo: '/404'});
     }
   ]);
 
