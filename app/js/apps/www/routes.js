9dd97192b7325b7154699562358bcb07b21dc745
Stephane Bilodeau <bilodeaustephane@gmail.com>
2014-10-05 15:16:03 +0900

    Add HLS route

--- app/js/apps/www/routes.js
+++ app/js/apps/www/routes.js
@@ -6,9 +6,10 @@ define(['app', 'providers/routes/extended-route-provider'], function(app) { 'use
       $locationProvider.hashPrefix('!');
 
       $routeProvider
-        .when('/npmop1/insession',           {templateUrl: '/app/views/meetings/documents/in-session.html',  resolveController: true, progress : { stop : false }, title : "NP COP-MOP 1 In In-session Documents", documentsUrl : "/doc/no-cache/npmop1/insession/" })
-        .when('/mop7/insession',             {templateUrl: '/app/views/meetings/documents/in-session.html',  resolveController: true, progress : { stop : false }, title : "BS COP-MOP 7 In-session Documents",    documentsUrl : "/doc/no-cache/mop7/insession/"   })
-        .when('/cop12/insession',            {templateUrl: '/app/views/meetings/documents/in-session.html',  resolveController: true, progress : { stop : false }, title : "COP 12 In-Session Documents",          documentsUrl : "/doc/no-cache/cop12/insession/"  })
+        .when('/npmop1/insession',           {templateUrl: '/app/views/meetings/documents/in-session.html',  resolveController: true, progress : { stop : false }, title : "NP COP-MOP 1 In In-session Documents", documentsUrl : "/doc/no-cache/npmop1/insession/"     })
+        .when('/mop7/insession',             {templateUrl: '/app/views/meetings/documents/in-session.html',  resolveController: true, progress : { stop : false }, title : "BS COP-MOP 7 In-session Documents",    documentsUrl : "/doc/no-cache/mop7/insession/"       })
+        .when('/cop12/insession',            {templateUrl: '/app/views/meetings/documents/in-session.html',  resolveController: true, progress : { stop : false }, title : "COP 12 In-Session Documents",          documentsUrl : "/doc/no-cache/cop12/insession/"      })
+        .when('/cop12/hls/insession',        {templateUrl: '/app/views/meetings/documents/in-session.html',  resolveController: true, progress : { stop : false }, title : "COP 12 High Level Segment Documents", documentsUrl : "/doc/no-cache/cop12/hls/insession/"  })
 
         .when('/printsmart/printshop',       {templateUrl: '/app/views/print-smart/printshop.html',  resolveController: true })
         .when('/404',                        {templateUrl: '/app/views/404.html',                   resolveUser: true })
