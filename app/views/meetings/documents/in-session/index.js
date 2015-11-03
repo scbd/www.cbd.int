c6a6ef8206b2d2127ce7ed32cc81cdba0bcc8627
Stephane Bilodeau <bilodeaustephane@gmail.com>
2015-11-03 09:42:41 -0500

    Rrefresh document count  every 2 minutes

--- app/views/meetings/documents/in-session/index.js
+++ app/views/meetings/documents/in-session/index.js
@@ -1,64 +1,87 @@
 define(['app', 'data/in-session/meetings', 'underscore'], function(app, meetingsData, _) { "use strict";
 
-	return ['$scope', '$http', function($scope, $http) {
+	return ['$scope', '$http', '$q', '$timeout', function($scope, $http, $q, $timeout) {
+
+        var refreshTimer = null;
 
 		$scope.meetings = _.where(_.clone(meetingsData, true), { list:true }); // deep clone;
+
+        loadInfo();
+        loadStats();
 
 		//========================================
 		//
 		// LOAD meetings info from db/index
 		//
 		//========================================
-		var query = {
-			q : 'schema_s:'+solrEscape('meeting')+' AND symbol_s:(' + _($scope.meetings).pluck('code').map(solrEscape).value().join(' ') + ')',
-			fl : 'symbol_s,title_t,startDate_dt,endDate_dt'
-		};
+        function loadInfo() {
 
-		$http.get('/api/v2013/index', {params : query }).then(function(res){
+            var query = {
+    			q : 'schema_s:'+solrEscape('meeting')+' AND symbol_s:(' + _($scope.meetings).pluck('code').map(solrEscape).value().join(' ') + ')',
+    			fl : 'symbol_s,title_t,startDate_dt,endDate_dt'
+    		};
 
-			var now = new Date();
+            return $http.get('/api/v2013/index', {params : query }).then(function(res){
 
-			$scope.meetings.forEach(function(meeting) {
+    			var now = new Date();
 
-				var m = _.findWhere(res.data.response.docs, { symbol_s : meeting.code });
+    			$scope.meetings.forEach(function(meeting) {
 
-				if(!m)
-					return;
+    				var m = _.findWhere(res.data.response.docs, { symbol_s : meeting.code });
 
-				meeting.name      = m.title_t;
-				meeting.startDate = new Date(m.startDate_dt);
-				meeting.endDate   = new Date(m.endDate_dt);
+    				if(!m)
+    					return;
 
-				var start = new Date(m.startDate_dt);
-				var end   = new Date(m.endDate_dt);
+    				meeting.name      = m.title_t;
+    				meeting.startDate = new Date(m.startDate_dt);
+    				meeting.endDate   = new Date(m.endDate_dt);
 
-				end.setDate(end.getDate()+1); // + 24h
+    				var start = new Date(m.startDate_dt);
+    				var end   = new Date(m.endDate_dt);
 
-				meeting.primary   = start < now && now < end;
-			});
-		});
+    				end.setDate(end.getDate()+1); // + 24h
 
+    				meeting.primary   = start < now && now < end;
+    			});
+    		});
+        }
 
 		//========================================
 		//
 		// LOAD meetings stats
 		//
 		//========================================
-		$scope.meetings.forEach(function(meeting) {
+        function loadStats() {
 
-			var query = {
-				q : {
-					meeting : meeting.code,
-					section : { $in : _.pluck(meeting.sections, 'code') },
-					locales : { $exists: true, $not: {$size: 0} }
-				},
-				c : 1
-			};
+            var promises = _.map($scope.meetings, function(meeting) {
 
-			$http.get('/api/v2015/insession-documents', { params : query }).then(function(res){
-				meeting.documentCount = res.data.count;
-			});
-		});
+    			var query = {
+    				q : {
+    					meeting : meeting.code,
+    					section : { $in : _.pluck(meeting.sections, 'code') },
+    					locales : { $exists: true, $not: {$size: 0} }
+    				},
+    				c : 1
+    			};
+
+    			return $http.get('/api/v2015/insession-documents', { params : query }).then(function(res){
+    				meeting.documentCount = res.data.count;
+    			});
+    		});
+
+            $q.all(promises).finally(function() {
+                $timeout.cancel(refreshTimer);
+                refreshTimer = $timeout(loadStats, 2*60*1000); //Every 2 minutes
+            });
+        }
+
+        //=============================================
+		//
+		//
+		//=============================================
+        $scope.$on("$destroy", function() {
+            $timeout.cancel(refreshTimer);
+        });
 
 		//========================================
 		//
