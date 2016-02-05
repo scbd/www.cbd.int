291bf9e02658384ff30c11cc80c31e9accf3d6df
Philippe Leblond <philippe.leblond@cbd.int>
2016-02-04 20:50:56 -0500

    no message

--- app/views/decisions/edit.js
+++ app/views/decisions/edit.js
@@ -8,8 +8,17 @@ define(['underscore', 'require', 'ngDialog', 'authentication'], function(_, requ
         $scope.save   = save;
         $scope.selectDecision = selectDecision;
         $scope.selectNotification = selectNotification;
+        $scope.actionEdit  = edit;
+        $scope.actionBox   = function(tag) { surroundSelection(tag); };
+        $scope.actionUnbox = function()    { unsurroundSelection(); };
+        $scope.element     = {};
 
         load();
+
+        $scope.$watch('element.type', tag);
+        $scope.$watch('element.paragraph', tag);
+        $scope.$watch('element.item', tag);
+        $scope.$watch('element.subitem', tag);
 
         return;
 
@@ -86,13 +95,16 @@ define(['underscore', 'require', 'ngDialog', 'authentication'], function(_, requ
             selectedElement = null;
         }
 
-        function editable() {
+        function edit() {
 
             var $div = $('#content'), isEditable = $div.is('.editable');
             $div.prop('contenteditable', !isEditable).toggleClass('editable');
+            $div.focus();
         }
 
         function selectNode(node) {
+
+            $(selectedElement).attr('data-info', JSON.stringify($scope.element));
 
             $(selectedElement).removeClass('selected');
 
@@ -100,7 +112,24 @@ define(['underscore', 'require', 'ngDialog', 'authentication'], function(_, requ
 
             $(selectedElement).addClass('selected');
 
+            $scope.element = $(selectedElement).data('info');
+            $scope.$applyAsync();
+
             event.stopPropagation();
+        }
+
+        function tag() {
+
+            if(selectedElement && $scope.element) {
+
+                var tag = $scope.element.type || '';
+
+                if($scope.element.paragraph) tag += ' ' + $scope.element.paragraph;
+                if($scope.element.item)      tag += '' + $scope.element.item;
+                if($scope.element.subitem)   tag += ' (' + $scope.element.subitem + ')';
+
+                $(selectedElement).attr('data-type', tag);
+            }
         }
 
         function clean(node) {
@@ -186,3 +215,34 @@ define(['underscore', 'require', 'ngDialog', 'authentication'], function(_, requ
         }
     }];
 });
+
+(function(){
+
+    var cache = [0],
+        expando = 'data' + +new Date();
+
+    function data(elem) {
+
+        var cacheIndex = elem[expando],
+            nextCacheIndex = cache.length;
+
+        if(!cacheIndex) {
+            cacheIndex = elem[expando] = nextCacheIndex;
+            cache[cacheIndex] = {};
+        }
+
+        return {
+            get : function(key) {
+                return cache[cacheIndex][key];
+            },
+            set : function(key, val) {
+                cache[cacheIndex][key] = val;
+                return val;
+            }
+        }
+
+    }
+
+    window.data = data;
+
+})();
