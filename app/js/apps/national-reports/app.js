1aa5f05f1b2e29e144252b59fb226ba3c5fc616d
Philippe Leblond <philippe.leblond@cbd.int>
2016-06-07 17:05:43 -0400

    Remove Bower Package "angular-growl" (was unused)

--- app/js/apps/national-reports/app.js
+++ app/js/apps/national-reports/app.js
@@ -2,25 +2,21 @@ define([
     'angular',
     'ngRoute',
     'ngCookies',
-    'angular-growl',
     'angularMoment',
     'ngAnimate',
     'bootstrap',
     'directives/ui-bootstrap-custom'], function(angular) { 'use strict';
 
-    var deps = ['ngRoute', 'ngCookies', 'ngAnimate','angular-growl', 'angularMoment', 'ui.bootstrap'];
+    var deps = ['ngRoute', 'ngCookies', 'ngAnimate', 'angularMoment', 'ui.bootstrap'];
 
     angular.defineModules(deps);
 
     var app = angular.module('app', deps);
 
-    app.config(['$httpProvider', 'growlProvider', function($httpProvider, growlProvider) {
+    app.config(['$httpProvider', function($httpProvider) {
 
         $httpProvider.useApplyAsync(true);
         $httpProvider.interceptors.push('authenticationHttpIntercepter');
-
-        growlProvider.globalTimeToLive(7000);
-        growlProvider.globalEnableHtml(false);
     }]);
 
     return app;
