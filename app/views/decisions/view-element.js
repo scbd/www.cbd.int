cfa71e44e4bbeb5103ea433eb3f412cc2d4c21ed
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-06-15 12:33:13 -0400

    Re-enabled filters + cosmetics

--- app/views/decisions/view-element.js
+++ app/views/decisions/view-element.js
@@ -1,118 +1,93 @@
-define(['app', 'text!./view-element.html'], function(app, html) { 'use strict';
+define(['app', 'text!./view-element.html', 'lodash'], function(app, html, _) { 'use strict';
 
-	return app.directive('info', ["$http", function($http) {
-
-        console.log('dataInfo registered')
+	return app.directive('info', [function() {
 		return {
 			restrict : "A",
-		//	replace : true,
 			template : html,
 			transclude: true,
-		//	require: '?^printSmart',
-			scope: true,//  {
-		//		documentsFn : "&documents",
-		//		tag : "@tag"
-		//	},
+			scope: {},
 			link: function ($scope, element, attrs) {
 
 				var info = JSON.parse(attrs.info);
 
-		//     if(info.type=='title') {
-		//         $(this).addClass('always');
-		//     }
-		//
-			    if(info.type=='sectionTitle') {
-			        element.addClass('sectionTitle');
-			    }
+                console.log(info);
+
+                if(info.data && info.data.type=='information')
+                    info.data.type = 'informational';
+
+				$scope.infoType = info.type;
+
+                $scope.$emit('registerElement', info);
 
 				if(info.type=='paragraph') {
-console.log(info)
+			        element.addClass('box');
+
 					$scope.type     = info.data.type;
 					$scope.session  = info.data.session;
 					$scope.decision = info.data.decision;
 					$scope.section  = info.data.section;
-					$scope.code     = info.data.paragraph;
+                    $scope.code     = pad(info.paragraph, '0', 2);
 					$scope.name     = 'paragraph ' + info.paragraph;
 					$scope.actors   = info.data.actors;
 					$scope.statuses = info.data.statuses;
 
-
-
-
 					if(info.item) {
 
-						var n = info.item.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+                        var n = info.item.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
 
-				        $scope.code += '.' + n;
+                        $scope.code = pad(info.paragraph, '0', 2) + '.' + pad(n, '0', 2);
 				        $scope.name += ' item ('+info.item+')';
 				    }
 				}
 
-				if($scope.type=='information') $scope.type = 'informational';
+                if(info.type=='title')        return;
+                if(info.type=='sectionTitle') return;
+
+                $scope.$on('filterElement', function (evt, filters) {
+
+                    element.removeClass('current');
+
+                    if(filters) element.addClass('xhide').removeClass('xshow');
+                    else        element.addClass('xshow').removeClass('xhide');
+
+                    if(info.type!='paragraph') {
+                        return;
+                    }
+
+                    if(!filters)
+                        return;
+
+                    var visible = true;
+
+                    if(visible && filters.actors)      visible = _(filters.actors     ).intersection( info.data.actors    ).some();
+                    if(visible && filters.statuses)    visible = _(filters.statuses   ).intersection( info.data.statuses  ).some();
+                    if(visible && filters.types)       visible = _(filters.types      ).intersection([info.data.type     ]).some();
+                    if(visible && filters.routeCodes)  visible = _(filters.routeCodes ).intersection([info.data.code     ]).some();
+
+                    if(visible) {
+                        element                                       .addClass('xshow').removeClass('xhide');
+                        element.parents()                             .addClass('xshow').removeClass('xhide');
+                        element.parents().find('element.sectionTitle').addClass('xshow').removeClass('xhide');
+                    }
+
+                    if(filters.routeCodes && ~filters.routeCodes.indexOf(info.data.code)) {
+                        element.addClass('current');
+                    }
+                });
 
 
-	            //     if(info.data && info.data.code==routeCode) {
-	            //         $(this).addClass('current');
-	            //         $scope.current = info;
-	            //     }
+                //==============================
+                //
+                //==============================
+                function pad(input, char, length) {
 
+                    var output = (input || '').toString();
 
+                    while(output.length<length)
+                        output = char + output;
 
-	            //	TO MOVE TO PARENT
-				//
-	            //     if(info.data && info.data.actors) {
-	            //         $scope.actors = _.union($scope.actors, info.data.actors);
-	            //     }
-	            //
-				//         if(info.data && info.data.statuses && info.data.statuses.includes('implemented')) $(this).addClass('implemented');
-	            //         if(info.data && info.data.statuses && info.data.statuses.includes('superseded')) $(this).addClass('superseded');
-	            //         if(info.data && info.data.statuses && info.data.statuses.includes('elapsed')) $(this).addClass('elapsed');
-	            //         if(info.data && info.data.statuses && info.data.statuses.includes('active')) $(this).addClass('active');
-
-
-
-	            //
-	            //         $(this).addClass('box');
-	            //
-	            //                 });
-	            //             }
-	            //
-	            //         }
-	            //     }
-	            //
-	            //
-	            //
-	            //     if($scope.list && info.data && info.data.statuses && info.data.statuses.includes('implemented'))
-	            //         $(this).prepend('<span class="pull-right label label-default" style="opacity:0.5;margin-right:6px"><i class="fa fa-info-circle" aria-hidden="true"></i> implemented</span> ');
-	            //     if($scope.list && info.data && info.data.statuses && info.data.statuses.includes('superseded'))
-	            //         $(this).prepend('<span class="pull-right label label-default" style="opacity:0.5;margin-right:6px"><i class="fa fa-info-circle" aria-hidden="true"></i> superseded</span> ');
-	            //     if($scope.list && info.data && info.data.statuses && info.data.statuses.includes('elapsed'))
-	            //         $(this).prepend('<span class="pull-right label label-default" style="opacity:0.5;margin-right:6px"><i class="fa fa-info-circle" aria-hidden="true"></i> elapsed</span> ');
-	            //     if($scope.list && info.data && info.data.statuses && info.data.statuses.includes('active'))
-	            //         $(this).prepend('<span class="pull-right label label-success" style="opacity:0.5;margin-right:6px"><i class="fa fa-info-circle" aria-hidden="true"></i> active</span> ');
-	            //
-	            //         if(info.paragraph && $scope.list && type=='operational')
-	            //             $(this).prepend('<span class="pull-right label label-info" style="opacity:0.5;"><i class="fa fa-cog" aria-hidden="true"></i> '+type+'</span>');
-	            //         if(info.paragraph && $scope.list && type=='informational')
-	            //             $(this).prepend('<span class="pull-right label label-default" style="opacity:0.5;"><i class="fa fa-info-circle" aria-hidden="true"></i> '+type+'</span>');
-	            //
-	            //
-	            //     // if(info.data)
-	            //     //     $(this).append(JSON.stringify(info.data));
-	            //
-	            //     if(type=='informational') $scope.infoCount++;
-	            //     if(type=='operational'  ) $scope.operCount++;
-	            //
-	            //     if(info.data && info.data.statuses && info.data.statuses.includes('implemented')) $scope.statusCounts.implemented++;
-	            //     if(info.data && info.data.statuses && info.data.statuses.includes('superseded'))  $scope.statusCounts.superseded++;
-	            //     if(info.data && info.data.statuses && info.data.statuses.includes('elapsed'))     $scope.statusCounts.elapsed++;
-	            //     if(info.data && info.data.statuses && info.data.statuses.includes('active'))      $scope.statusCounts.active++;
-	            //
-	            //
-	            // });
-	            //
-	            // if(!$scope.list)
-	            //     $scope.filter('current', 1);
+                    return output;
+                }
 			}
 		};
 	}]);
--- app/views/decisions/view.js
+++ app/views/decisions/view.js
@@ -1,196 +1,242 @@
 define(['app', 'underscore', './view-element'], function(app, _) { 'use strict';
 
-    return ['$scope', '$http', '$route', '$location', '$filter', '$q', '$compile', function($scope, $http, $route, $location, $filter, $q, $compile) {
+    return ['$scope', '$http', '$route', '$location', '$filter', '$q', '$compile', '$window', function($scope, $http, $route, $location, $filter, $q, $compile, $window) {
 
-        $scope.session      = $route.current.params.session;
-        $scope.session0     = $scope.session>9 ? '' : '0' + $scope.session;
-        $scope.element      = $route.current.params.element;
-        $scope.infoCount    = 0;
-        $scope.operCount    = 0;
-        $scope.statusCounts = { implemented: 0, superseded: 0, elapsed: 0, active: 0 };
-        $scope.actors       = [];
+        var elements  = $route.current.params.element.match(/^([0-9]+)(?:\.([A-Z]*)([0-9]+)(?:\.([0-9]+))?)?/);
+        var session   = $route.current.params.session;
+        var decision  = (elements[1]||'').replace(/^0+/, '');
+        var section   = (elements[2]||'').replace(/^0+/, '').toUpperCase();
+        var paragraph = (elements[3]||'').replace(/^0+/, '');
+        var item      = (elements[4]||'').replace(/^0+/, '');
+        var routeCode;
 
-        $scope.list = !$route.current.params.element.includes('.');
+        if(section||paragraph) {
 
-        var elements  = $route.current.params.element.match(/^([0-9]+)(?:\.([0-9]+)(?:\.([0-9]+))?)?/);
-        var routeCode = 'CBD/COP/'+$scope.session0;
+            routeCode  = 'CBD/COP/'+pad($route.current.params.session) +'/';
+            routeCode += pad(decision);
+            routeCode += '.' + section + pad(paragraph);
 
-        var elementLevel1 = elements[1] ? elements[1] : '0';
-        var elementLevel2 = elements[2] ? elements[2] : '0';
-        var elementLevel3 = elements[3] ? elements[3] : '0';
+            if(item)
+                routeCode += '.' + pad(item);
+        }
 
-        if(elementLevel1.length<2) elementLevel1 = '0' + elementLevel1;
-        if(elementLevel2.length<2) elementLevel2 = '0' + elementLevel2;
-        if(elementLevel3.length<2) elementLevel3 = '0' + elementLevel3;
+        $scope.romanize      = romanize;
+        $scope.pad0          = pad;
+        $scope.sumValues     = sumValues;
+        $scope.filter        = filter;
+        $scope.toggleFilters = toggleFilters
+        $scope.list          = !routeCode;
+        $scope.session       = session;
+        $scope.decision      = parseInt(decision);
+        $scope.section       = section;
+        $scope.paragraph     = paragraph;
+        $scope.item          = item ? String.fromCharCode(96+parseInt(elements[4])) : '';
+        $scope.typeCounts    = { informational: 0, operational: 0 };
+        $scope.statusCounts  = { implemented: 0, superseded: 0, elapsed: 0, active: 0 };
+        $scope.actorCounts   = { };
+        $scope.actors        = [];
+        $scope.$root.page    = { title: 'Decision '+romanize(session)+'/'+decision };
 
-        if(elementLevel1!='00') routeCode += '/' + elementLevel1;
-        if(elementLevel2!='00') routeCode += '.' + elementLevel2;
-        if(elementLevel3!='00') routeCode += '.' + elementLevel3;
+        if(!$scope.list) {
 
-        $scope.decision  = parseInt(elementLevel1);
-        $scope.decision0 = elementLevel1;
+            $scope.$root.page.title = '';
 
-        $http.get('https://api.cbd.int/api/v2015/tests?q={%22decision%22:%22'+romanize($scope.session)+'/'+$scope.decision+'%22}&fo=1'/*, { params : query }*/).then(function(res) {
+            if(section)   $scope.$root.page.title += ' section ' + section;
+            if(paragraph) $scope.$root.page.title += ' paragraph ' + paragraph;
+            if(item)      $scope.$root.page.title += ' item ('+String.fromCharCode(96+parseInt(item))+')';
+        }
+
+        var registeredElements = [];
+
+
+        //==============================
+        //
+        //==============================
+        $http.get('https://api.cbd.int/api/v2015/tests', { params : { q: { decision : romanize($scope.session)+'/'+$scope.decision }, fo: 1 } }).then(function(res) {
 
             var link    = $compile(res.data.content);
             var content = link($scope);
 
             $('#content').html(content);
 
-            //$('#content').html();
+        }).then(function(){
 
-            // $('#content element[data-info]').each(function () {
-            //
-            //     var info = JSON.parse($(this).attr('data-info'));
-            //
-            //     if(info.type=='title') {
-            //         $(this).addClass('always');
-            //     }
-            //
-            //     if(info.type=='sectionTitle') {
-            //         $(this).addClass('sectionTitle');
-            //     }
-            //
-            //     var name = 'paragraph '+info.paragraph;
-            //     var code = info.paragraph;
-            //     var type = info.data ? info.data.type : '';
-            //
-            //     if(type=='information') type = 'informational';
-            //
-            //     if(info.data && info.data.code==routeCode) {
-            //         $(this).addClass('current');
-            //         $scope.current = info;
-            //     }
-            //
-            //     if(info.data && info.data.actors) {
-            //         $scope.actors = _.union($scope.actors, info.data.actors);
-            //     }
-            //
-            //     if(info.type=='secApara') { code = 'A' + code; name = 'sec. A ' + name; }
-            //     if(info.type=='secBpara') { code = 'B' + code; name = 'sec. B ' + name; }
-            //     if(info.type=='secCpara') { code = 'C' + code; name = 'sec. C ' + name; }
-            //     if(info.type=='secDpara') { code = 'D' + code; name = 'sec. D ' + name; }
-            //     if(info.type=='secEpara') { code = 'E' + code; name = 'sec. E ' + name; }
-            //
-            //     if(info.item) {
-            //
-            //         var n = info.item.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
-            //
-            //         code += '.' + n;
-            //         name += ' item ('+info.item+')';
-            //     }
-            //
-            //
-            //     if(info.paragraph) {
-            //
-            //
-            //         $(this).addClass(type);
-            //
-            //         if(info.data && info.data.statuses && info.data.statuses.includes('implemented')) $(this).addClass('implemented');
-            //         if(info.data && info.data.statuses && info.data.statuses.includes('superseded')) $(this).addClass('superseded');
-            //         if(info.data && info.data.statuses && info.data.statuses.includes('elapsed')) $(this).addClass('elapsed');
-            //         if(info.data && info.data.statuses && info.data.statuses.includes('active')) $(this).addClass('active');
-            //
-            //         $(this).addClass('box');
-            //
-            //         if($scope.list) {
-            //             $(this).prepend('<p><a type="button" class="btn btn-primary btn-xs" href="/decisions/cop/'+$scope.session+'/'+$scope.decision+'.'+code+'"><i class="fa fa-search" aria-hidden="true"></i> '+name+'</a></button></p>');
-            //
-            //             if(info.data && info.data.actors) {
-            //                 var self = this;
-            //                 info.data.actors.forEach(function (actor) {
-            //                     $(self).addClass('actor-'+actor);
-            //                     if(actor=='executive-secretary') actor = 'es';
-            //                     if(actor=='Others') actor = 'others';
-            //                     $(self).prepend('<span class="pull-right label label-default" style="opacity:0.5;margin-right:6px"><i class="fa fa-user" aria-hidden="true"></i> '+actor+'</span> ');
-            //                 });
-            //             }
-            //
-            //         }
-            //     }
-            //
-            //
-            //
-            //     if($scope.list && info.data && info.data.statuses && info.data.statuses.includes('implemented'))
-            //         $(this).prepend('<span class="pull-right label label-default" style="opacity:0.5;margin-right:6px"><i class="fa fa-info-circle" aria-hidden="true"></i> implemented</span> ');
-            //     if($scope.list && info.data && info.data.statuses && info.data.statuses.includes('superseded'))
-            //         $(this).prepend('<span class="pull-right label label-default" style="opacity:0.5;margin-right:6px"><i class="fa fa-info-circle" aria-hidden="true"></i> superseded</span> ');
-            //     if($scope.list && info.data && info.data.statuses && info.data.statuses.includes('elapsed'))
-            //         $(this).prepend('<span class="pull-right label label-default" style="opacity:0.5;margin-right:6px"><i class="fa fa-info-circle" aria-hidden="true"></i> elapsed</span> ');
-            //     if($scope.list && info.data && info.data.statuses && info.data.statuses.includes('active'))
-            //         $(this).prepend('<span class="pull-right label label-success" style="opacity:0.5;margin-right:6px"><i class="fa fa-info-circle" aria-hidden="true"></i> active</span> ');
-            //
-            //         if(info.paragraph && $scope.list && type=='operational')
-            //             $(this).prepend('<span class="pull-right label label-info" style="opacity:0.5;"><i class="fa fa-cog" aria-hidden="true"></i> '+type+'</span>');
-            //         if(info.paragraph && $scope.list && type=='informational')
-            //             $(this).prepend('<span class="pull-right label label-default" style="opacity:0.5;"><i class="fa fa-info-circle" aria-hidden="true"></i> '+type+'</span>');
-            //
-            //
-            //     // if(info.data)
-            //     //     $(this).append(JSON.stringify(info.data));
-            //
-            //     if(type=='informational') $scope.infoCount++;
-            //     if(type=='operational'  ) $scope.operCount++;
-            //
-            //     if(info.data && info.data.statuses && info.data.statuses.includes('implemented')) $scope.statusCounts.implemented++;
-            //     if(info.data && info.data.statuses && info.data.statuses.includes('superseded'))  $scope.statusCounts.superseded++;
-            //     if(info.data && info.data.statuses && info.data.statuses.includes('elapsed'))     $scope.statusCounts.elapsed++;
-            //     if(info.data && info.data.statuses && info.data.statuses.includes('active'))      $scope.statusCounts.active++;
-            //
-            //
-            // });
-            //
-            // if(!$scope.list)
-            //     $scope.filter('current', 1);
+            var filter;
+
+            if(routeCode) {
+                filter = { routeCodes : [routeCode] };
+            }
+
+            $scope.filter(filter, true);
+
+            if(!$scope.list)
+                $window.scrollTo(0,0);
         });
 
-        if(!$scope.list) {
-            window.scrollTo(0, 0);
 
-            $scope.paragraph = parseInt(elementLevel2);
 
-            if(elementLevel3!='00')
-                $scope.paragraph += ' item ('+String.fromCharCode(96+parseInt(elementLevel3))+')';
+        //==============================
+        //
+        //==============================
+        $scope.$on('registerElement', function (evt, info) {
+
+            evt.stopPropagation();
+
+            if(info.type !='paragraph')
+                return;
+
+            $scope.actors = _.union($scope.actors, info.data.actors);
+
+            if(info.data.code == routeCode)
+                $scope.current = info;
+
+            registeredElements.push(info);
+        });
+
+        //==============================
+        //
+        //==============================
+        function filter(filters) {
+
+            $scope.filters = filters;
+
+            $scope.$broadcast("filterElement", $scope.filters);
+
+            $('#content element.xshow').slideDown({ duration: 250, queue: false });
+            $('#content element.xhide').slideUp  ({ duration: 250, queue: false });
+
+            updateSums();
         }
 
-        if($scope.list) $scope.$root.page   = { title: 'Decision '+romanize($scope.session)+'/'+$scope.decision };
-        else            $scope.$root.page   = { title: 'Paragraph '+$scope.paragraph };
+        //==============================
+        //
+        //==============================
+        function toggleFilters(newFilters) {
 
-        $scope.filter = function (cls, duration) {
+            var filters;
+            var oldFilters = $scope.filters || {};
 
-            $scope.filtered = true;
+            newFilters = newFilters || {};
 
-            $('#content element').removeClass('xshow');         // reset
-            $('#content element.'+cls).addClass('xshow');
-            $('#content element.xshow element').addClass('xshow');
-            $('#content element.always').addClass('xshow');
-            $("#content element").has("element.xshow").addClass("xshow");
-            $('#content element.xshow element.sectionTitle').addClass('xshow');
+            var keys = _.union(_.keys(oldFilters), _.keys(newFilters));
 
-            $('#content element').addClass('xhide');            // reset
-            $('#content element.xshow').removeClass('xhide');
+            keys.forEach(function(key) {
 
-            $("#content element").has("element.xshow").show();
-            $('#content element.xshow').slideDown({ duration: duration||250, queue: false });
-            $('#content element.xhide').slideUp  ({ duration: duration||250, queue: false });
+                var o = oldFilters[key] || [];
+                var n = newFilters[key] || [];
+                var r = _(_.difference(o,n)).union(_.difference(n,o)).compact().value();
+
+                if(newFilters[key]===null)
+                    r = null;
+
+                if(!_.isEmpty(r)) {
+                    filters = filters || {};
+                    filters[key] = r;
+                }
+            });
+
+            filter(filters);
         }
 
-        $scope.removeFilter = function () {
+        //==============================
+        //
+        //==============================
+        function updateSums() {
 
-            $scope.filtered = false;
+            var filters, entries;
 
-            $('#content element').removeClass('xhide');         // reset
-            $('#content element').addClass('xshow');
-            $('#content element.xshow').slideDown(250);
+            $scope.actors.forEach(function(a) {
+                $scope.actorCounts[a] = $scope.actorCounts[a] || 0;
+            });
+
+            resetSum($scope.typeCounts);
+            resetSum($scope.statusCounts);
+            resetSum($scope.actorCounts);
+
+            filters = _.omit($scope.filters, 'types');
+            entries = _.filter(registeredElements, function(e) { return isMatch(e, filters); });
+            entries.forEach(function(e){
+                sum($scope.typeCounts, [e.data.type]);
+            });
+
+            filters = _.omit($scope.filters, 'statuses');
+            entries = _.filter(registeredElements, function(e) { return isMatch(e, filters); });
+            entries.forEach(function(e){
+                sum($scope.statusCounts, e.data.statuses);
+            });
+
+            filters = _.omit($scope.filters, 'actors');
+            entries = _.filter(registeredElements, function(e) { return isMatch(e, filters); });
+            entries.forEach(function(e){
+                sum($scope.actorCounts, e.data.actors);
+            });
+
         }
 
-        $scope.romanize = romanize;
+        //==============================
+        //
+        //==============================
+        function sum(container, entries) {
 
-        // $scope.decisions = decisions;
+            _.forEach(entries, function(entry) {
+                container[entry] = container[entry] || 0;
+                container[entry]++;
+            });
+        }
 
+        //==============================
+        //
+        //==============================
+        function sumValues(o) {
+            return _(o||{}).values().reduce(function(a,v) { return a+v}, 0);
+        }
+
+        //==============================
+        //
+        //==============================
+        function isMatch(entry, filters) {
+
+            if(!filters)
+                return true;
+
+            var match = true;
+
+            if(match && filters.actors)   match = _(filters.actors  ).intersection( entry.data.actors  ).some();
+            if(match && filters.statuses) match = _(filters.statuses).intersection( entry.data.statuses).some();
+            if(match && filters.types)    match = _(filters.types   ).intersection([entry.data.type]   ).some();
+
+            return match;
+        }
+
+        //==============================
+        //
+        //==============================
+        function resetSum(container) {
+            for(var k in container)
+                container[k] = 0;
+
+            return container;
+        }
+
+        //==============================
+        //
+        //==============================
         function romanize (n) {
-            var roman = [ '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXII', 'XXIV', 'XXV' ];
+            var roman = [ '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXII', 'XXIV', 'XXV', 'XXVI', 'XXVII', 'XXVIII', 'XXIX', 'XXX' ];
             return roman[n];
+        }
+
+        //==============================
+        //
+        //==============================
+        function pad(input) {
+
+            var output = (input || '').toString();
+
+            while(output.length<2)
+                output = '0' + output;
+
+            return output;
         }
     }];
 });
