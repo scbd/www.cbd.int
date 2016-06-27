17fd6f7d951d8e10c632a939d063f88bfcad55de
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-06-27 16:53:16 -0400

    Bound to decision data: Formatting, Actors, Type, Status, Notifications

---
+++ app/views/decisions/select-actors-list.js
@@ -0,0 +1,17 @@
+define([], function() {
+
+    return [
+        { code : 'parties' ,              title: 'Parties' },
+        { code : 'other-governments' ,    title: 'Other Government(s)' },
+        { code : 'executive-secretary' ,  title: 'Executive Secretary' },
+
+        { code : 'SBSTTA' ,               title: 'SBSTTA',     group : 'Subsidiary Body' },
+        { code : 'WGRI' ,                 title: 'WGRI/SBI',   group : 'Subsidiary Body' },
+        { code : 'WG8J' ,                 title: 'WG8J',       group : 'Subsidiary Body' },
+        { code : 'WGABS' ,                title: 'WGABS',      group : 'Subsidiary Body' },
+
+        { code : 'GEF' ,                  title: 'Global Environment Facility' , group : 'Others' },
+        { code : 'IPLC' ,                 title: 'ILPC' ,                        group : 'Others' },
+        { code : 'Others' ,               title: 'Other stakeholder(s)' ,        group : 'Others' },
+    ];
+});
---
+++ app/views/decisions/select-statuses-list.js
@@ -0,0 +1,9 @@
+define([], function() {
+
+    return [
+        { code : 'active' ,      title: 'Active' },
+        { code : 'implemented' , title: 'Implemented' },
+        { code : 'elapsed' ,     title: 'Elapsed' },
+        { code : 'superseded' ,  title: 'Superseded' }
+    ];
+});
