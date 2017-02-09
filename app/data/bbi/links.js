1f7775aa4e200fd813b8f8f27a50b86dd75cc40b
Randy Houlahan <randy.houlahan@cbd.int>
2017-02-09 09:00:04 -0500

    BBI data

---
+++ app/data/bbi/links.js
@@ -0,0 +1,12 @@
+define(['text!./links.json'], function(rawData) {
+
+    try
+    {
+        return JSON.parse(rawData);
+    }
+    catch(e)
+    {
+        console.error("Errro parsing links.json: "+e);
+
+    }
+});
