a036a43b35b4dfeab74e6e3ca7a64ce5a8fc5c0c
Randy Houlahan <randy.houlahan@cbd.int>
2017-02-27 09:32:40 -0500

    resolve user

--- app/views/bbi/platform.js
+++ app/views/bbi/platform.js
@@ -1,10 +1,11 @@
 define(['app', 'lodash','data/bbi/links-platform', 'directives/bbi/crumbs', 'directives/bbi/menu'], function(app, _,links) { 'use strict';
 
-	return ['$location','$scope', function ($location,$scope) {
+	return ['$location','$scope','user', function ($location,$scope,user) {
 
         var _ctrl = this;
 				_ctrl.links=links.links;
 				_ctrl.goTo = goTo;
+				_ctrl.user=user;
 				$scope.$root.page={};
 				$scope.$root.page.title = "BBI Web Platform";
 				//============================================================
