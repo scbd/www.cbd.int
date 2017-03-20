62da19182731a857c812d6c72943155f7cfe86f1
Randy Houlahan <randy.houlahan@cbd.int>
2017-03-20 09:57:45 -0400

    ensure users can view edit abs orgs

--- app/providers/realm.js
+++ app/providers/realm.js
@@ -15,17 +15,27 @@ define(['app', 'angular'], function (app, angular) { 'use strict';
 
 		return {
 			request: function(config) {
+        if(config && config.params && config.params.skipRealmHeader){
+                   config.params.skipRealmHeader   = undefined;
+                   config.headers.realm            = undefined;
+               }
+               else if(config && config.header && config.headers.realm === undefined){
+                   delete config.headers.realm;
+               }
+
+               if((config.headers || {}).hasOwnProperty('realm'))
+                   return config;
+
 				var trusted = /^https:\/\/api.cbd.int\//i .test(config.url) ||
 						      /^https:\/\/localhost[:\/]/i.test(config.url) ||
 							  /^\/\w+/i                   .test(config.url);
 
                 //exception if the APi call needs to be done for different realm
-                if(trusted && realm && config.params && config.params.realm && config.params.realm != realm) {
-                      config.headers = angular.extend(config.headers || {}, { realm : config.params.realm });
-                }
-                else if(trusted && realm ) {
-                    config.headers = angular.extend(config.headers || {}, { realm : realm });
-                }
+                if (trusted && realm) {
+                       config.headers = angular.extend(config.headers || {}, {
+                           realm: realm.value || realm
+                       });
+                   }
 
                 return config;
 			}
