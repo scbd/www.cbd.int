6377710ee85a5490118a56b1c8d2bf5e2642d430
Randy Houlahan <randy.houlahan@cbd.int>
2017-04-04 13:24:26 -0400

    IDB celebrations country list

---
+++ app/directives/idb-celebrations/menu-vertical.js
@@ -0,0 +1,53 @@
+define(['app', 'text!./menu-vertical.html','providers/locale','ngSmoothScroll'], function(app, templateHtml) { 'use strict';
+
+    //============================================================
+    //
+    //============================================================
+    app.directive('menuVertical',["locale","$window",'smoothScroll',  function(locale,$window,smoothScroll) {
+        return {
+            restrict: 'E',
+            template : templateHtml,
+            scope: {
+                links: '=links',
+                pull: '@?',
+                search: '@?',
+            },
+            link: function ($scope) {
+              $scope.locale=locale;
+            },
+            controller: function ($scope, $location) {
+                // ============================================================
+                //
+                // ============================================================
+                $scope.goToLink = function (link) {
+                    if(link.children.length===0) {
+                        $window.location.href=link.source;
+                    }
+                };
+                // ============================================================
+                //
+                // ============================================================
+                $scope.textSearch = function (keywords) {
+
+                      if(!keywords)return;
+                      $location.path('/platform/search');
+                      $location.replace();
+                      $location.search('keywords',[keywords]);
+
+                };
+                // ============================================================
+                //
+                // ============================================================
+                $scope.goTo = function (link,event) {
+
+                  if(event)
+                    event.preventDefault();
+
+                    $location.path(link);
+
+                };
+            }
+        };
+    }]);
+
+});
