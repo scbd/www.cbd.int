9a0ddb763cd854cc6b60bf76d5a38d2b9c00c8cd
Stephane Bilodeau <bilodeaustephane@gmail.com>
2017-11-14 11:36:55 -0500

    Update liste of Actors

--- app/views/decisions/data/actors.js
+++ app/views/decisions/data/actors.js
@@ -2,17 +2,41 @@ define([], function() {
 
     return [
         { code : 'parties' ,              title: 'Parties' },
+        { code : 'governments' ,          title: 'Government(s)' },
         { code : 'other-governments' ,    title: 'Other Government(s)' },
-        { code : 'executive-secretary' ,  title: 'Executive Secretary' },
+        { code : 'executive-secretary' ,  title: 'Executive Secretary of the Convention on Biological Diversity' },
 
         { code : 'SBSTTA' ,               title: 'SBSTTA',     group : 'Subsidiary Body' },
         { code : 'SBI' ,                  title: 'SBI',        group : 'Subsidiary Body' },
         { code : 'WGRI' ,                 title: 'WGRI',       group : 'Subsidiary Body' },
         { code : 'WG8J' ,                 title: 'WG8J',       group : 'Subsidiary Body' },
         { code : 'WGABS' ,                title: 'WGABS',      group : 'Subsidiary Body' },
+        { code : 'WGABS' ,                title: 'WGABS',      group : 'Subsidiary Body' },
 
-        { code : 'GEF' ,                  title: 'Global Environment Facility' , group : 'Others' },
-        { code : 'IPLC' ,                 title: 'ILPC' ,                        group : 'Others' },
-        { code : 'Others' ,               title: 'Other stakeholder(s)' ,        group : 'Others' },
+        { code : 'GEF' ,                     title: 'Global Environment Facility',                                                             group : 'UN Agencies and Conventions' },
+        { code : 'IPLC' ,                    title: 'Indigenous Peoples and Local Communities',                                                group : 'UN Agencies and Conventions' },
+        { code : 'UNGA',                     title: 'United Nations General Assembly',                                                         group : 'UN Agencies and Conventions' },
+        { code : 'UNEP',                     title: 'United Nations Environment Programme',                                                    group : 'UN Agencies and Conventions' },
+        { code : 'executive-director' ,      title: 'Executive Director of the United Nations Environment Programme',                          group : 'UN Agencies and Conventions' },
+        { code : 'FAO',                      title: 'Food and Agriculture Organization of the United Nations',                                 group : 'UN Agencies and Conventions' },
+        { code : 'UNESCO',                   title: 'United Nations Educational, Scientific and Cultural Organization',                        group : 'UN Agencies and Conventions' },
+        { code : 'WTO',                      title: 'World Trade Organization',                                                                group : 'UN Agencies and Conventions' },
+        { code : 'WIPO',                     title: 'World Intellectual Property Organization',                                                group : 'UN Agencies and Conventions' },
+        { code : 'CITES',                    title: 'Convention on International Trade in Endangered Species of Wild Fauna and Flora (CITES)', group : 'UN Agencies and Conventions' },
+        { code : 'CMS',                      title: 'Convention on the Conservation of Migratory Species of Wild Animals',                     group : 'UN Agencies and Conventions' },
+        { code : 'ITPGRFA',                  title: 'The International Treaty on Plant Genetic Resources for Food and Agriculture',            group : 'UN Agencies and Conventions' },
+        { code : 'RAMSAR',                   title: 'Convention on Wetlands',                                                                  group : 'UN Agencies and Conventions' },
+        { code : 'WHC',                      title: 'World Heritage Convention',                                                               group : 'UN Agencies and Conventions' },
+        { code : 'IPPC',                     title: 'International Plant Protection Convention',                                               group : 'UN Agencies and Conventions' },
+
+        { code : 'organizations',            title: 'Relevant organizations',                      group : 'Others' },
+        { code : 'economic-organizations',   title: 'Regional economic integration organizations', group : 'Others' },
+        { code : 'int-organizations',        title: 'International organizations',                 group : 'Others' },
+        { code : 'inter-gov-organizations',  title: 'Intergovernmental organizations',             group : 'Others' },
+        { code : 'private-sector',           title: 'Private sector',                              group : 'Others' },
+        { code : 'donors',                   title: 'Donors',                                      group : 'Others' },
+        { code : 'Businesses',               title: 'Businesses',                                  group : 'Others' },
+        { code : 'Others' ,                  title: 'Stakeholder(s)' ,                             group : 'Others' },
+
     ];
 });
