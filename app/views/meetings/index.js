862a77027c3a71886889b1072df237ad45cc7e87
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-11-29 17:25:09 -0500

    dynamic meeting page

---
+++ app/views/meetings/index.js
@@ -0,0 +1,12 @@
+﻿define(['app', 'lodash', 'services/meeting-service'], function(app, _) { 'use strict';
+
+return ['$location','$scope','$document', 'meetingService', '$route', '$q', '$sce',
+        function ($location,$scope,$document, meetingService, $route, $q, $sce) {
+       
+			var _ctrl = this;
+
+            $scope.trustedHtml = function (plainText) {
+                return $sce.trustAsHtml(plainText);
+            }
+    }];
+});
