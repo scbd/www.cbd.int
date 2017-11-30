7a20a7c60a399c160da01bc23b9eb4c47c91ad18
Blaise Fonseca <blaisefonseca@hotmail.com>
2017-11-30 11:06:45 -0500

    update browser url when meeting code is missing

--- app/views/meetings/index.js
+++ app/views/meetings/index.js
@@ -1,12 +1,19 @@
 ﻿define(['app', 'lodash', 'services/meeting-service'], function(app, _) { 'use strict';
 
-return ['$location','$scope','$document', 'meetingService', '$route', '$q', '$sce',
-        function ($location,$scope,$document, meetingService, $route, $q, $sce) {
+return ['$location','$scope','$timeout', '$route', '$sce', 
+        function ($location,$scope,$timeout,  $route, $sce) {
        
 			var _ctrl = this;
 
             $scope.trustedHtml = function (plainText) {
                 return $sce.trustAsHtml(plainText);
+            }
+
+            if(!$route.current.params.code){
+                $timeout(function(){
+                    if($scope.$parent.meeting)
+                        $location.path('/'+ $scope.$parent.meeting.code);                    
+                }, 500)
             }
     }];
 });
