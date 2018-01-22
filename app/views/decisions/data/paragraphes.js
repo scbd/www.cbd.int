a84c3993006f13914d53b4ddf2df0a7f7ed8aca4
Nancy Liang <nancy.liang@cbd.int>
2018-01-22 14:56:55 -0500

    update the limit of paragraph in a decision

--- app/views/decisions/data/paragraphes.js
+++ app/views/decisions/data/paragraphes.js
@@ -2,7 +2,7 @@ define([], function() {
 
     var items = [];
 
-    for(var i=1; i<=100; ++i) {
+    for(var i=1; i<=200; ++i) {
         items.push({
             code : i.toString(),
             value: i,
