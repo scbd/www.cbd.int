4d5ce9e7fd17304a9d40e7fa43e4877a1e02ce11
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-02-13 16:41:41 -0500

    remove conference header from template

--- app/template.js
+++ app/template.js
@@ -1,23 +1,15 @@
 define(['app', 'angular','text!./toast.html', 'text!./template-header.html', 'text!./template-footer.html',
-'lodash', 'services/conference-service', 'providers/realm'], function(app, ng, toastTemplate,headerHtml, footerHtml,_) {
+'lodash', 'providers/realm'], function(app, ng, toastTemplate,headerHtml, footerHtml,_) {
     'use strict';
 
-    app.directive('templateHeader', ['$rootScope', '$window', '$browser', '$document', 'authentication', '$q','toastr','$templateCache', '$http', 'conferenceService', '$route',
-                             function($rootScope,   $window,   $browser,   $document,   authentication,   $q,toastr,$templateCache, $http, conferenceService, $route) {
+    app.directive('templateHeader', ['$rootScope', '$window', '$browser', '$document', 'authentication', '$q','toastr','$templateCache', 
+                             function($rootScope,   $window,   $browser,   $document,   authentication,   $q,toastr,$templateCache) {
         return {
             restrict: 'E',
             template: headerHtml,
             link: function(scope, elem) {},
-            controller: function($scope, $location, $route) {
+            controller: ['$scope', '$location', function($scope, $location) {
 
-                angular.element(document).ready(function () {
-
-                    $q.when(conferenceService.getActiveConference())
-                    .then(function(meeting){
-                        $scope.meeting = meeting;
-                    });   
-
-                });
                 $templateCache.put("directives/toast/toast.html", toastTemplate);
                 var basePath = (ng.element('base').attr('href')||'').replace(/\/+$/g, '');
 
@@ -68,47 +60,6 @@ define(['app', 'angular','text!./toast.html', 'text!./template-header.html', 'te
                 $scope.$on('signOut', function(){
                     $window.location.href = '/';
                 });
-
-                $scope.meetingNavCtrl = {
-                    fullPath : function(name) {
-                        return basePath + $location.path();
-                    },
-
-                    isSelected : function(name, exact) {
-
-                        if(name && $scope.meetingNavCtrl.currentSelection)
-                            return name==$scope.meetingNavCtrl.currentSelection;
-
-                        var selected = false;
-                        var path = basePath + $location.path();
-      
-                        //handle legacy redirect for /2016/mop-08/documents
-                        if(/^\/conferences\/2016\/mop-08/.test(path) && /^\/conferences\/2016\/cp-mop-08/.test(name))
-                            selected = true;
-                                                        
-                        if(exact) selected = selected || path === name;
-                        else if(name) selected = selected || path.indexOf(name)===0;
-                        else     selected = selected || path.indexOf('/conferences/')===0;
-
-                        return selected;
-                    },
-                    isMajorEventSelected : function(){
-                        if(!$scope.meeting)
-                            return;
-                        var isSelected = false; 
-                        
-                        for(var i=0; i<$scope.meeting.conference.events.length; i++){
-                            isSelected = $scope.meetingNavCtrl.isSelected('/conferences/'+$scope.meeting.code+
-                                        '/'+$scope.meeting.conference.events[i].code+'/')
-                            if(isSelected)
-                                break;
-                        }                
-                        return isSelected;
-                    },
-                    hash : function() {
-                        return $location.hash();
-                    }
-                };                
                 
                 //============================================================
                 //
@@ -163,34 +114,34 @@ define(['app', 'angular','text!./toast.html', 'text!./template-header.html', 'te
                     return (code||'').replace(/\-([a-z]{3})\-.*/, '').toUpperCase();
                 }
                 //==============================
-      //
-      //==============================
-      $rootScope.$on("showInfo", function(evt, msg) {
-          toastr.info(msg);
-      });
+                //
+                //==============================
+                $rootScope.$on("showInfo", function(evt, msg) {
+                    toastr.info(msg);
+                });
 
-      //==============================
-      //
-      //==============================
-      $rootScope.$on("showWarning", function(evt, msg) {
-          toastr.warning(msg);
-      });
+                //==============================
+                //
+                //==============================
+                $rootScope.$on("showWarning", function(evt, msg) {
+                    toastr.warning(msg);
+                });
 
-      //==============================
-      //
-      //==============================
-      $rootScope.$on("showSuccess", function(evt, msg) {
-          toastr.success(msg);
-      });
+                //==============================
+                //
+                //==============================
+                $rootScope.$on("showSuccess", function(evt, msg) {
+                    toastr.success(msg);
+                });
 
-      //==============================
-      //
-      //==============================
-      $rootScope.$on("showError", function(evt, msg) {
-          toastr.error(msg);
-      });
+                //==============================
+                //
+                //==============================
+                $rootScope.$on("showError", function(evt, msg) {
+                    toastr.error(msg);
+                });
 
-              }
+            }]
         };
     }]);
 
