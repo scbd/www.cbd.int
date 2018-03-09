4584c67b5765cf8867a45a54635e8a8b79835204
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-03-09 10:34:02 -0500

    create article service

---
+++ app/services/article-service.js
@@ -0,0 +1,19 @@
+﻿define(['app'], function (app) {
+
+    app.factory("articleService", ['$http', '$q', function ($http, $q) {
+
+            function getArticle(articleId){
+                
+                return $q.when($http.get('/api/v2017/articles/' + articleId, {cache:true}))
+                        .then(function(data){
+                            return data.data;
+                        });
+                
+            }
+            return {
+                getArticle    : getArticle
+            }
+
+    }]);
+
+});
