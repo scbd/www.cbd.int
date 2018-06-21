619b7eeda4986cab973a22aaf567daca81b6f3f3
Randy Houlahan <randy.houlahan@cbd.int>
2018-06-21 09:30:19 -0400

    media mockup

--- app/services/conference-service.js
+++ app/services/conference-service.js
@@ -1,13 +1,13 @@
 ﻿define(['app', 'lodash', 'moment-timezone'], function (app, _, moment) {
 
-    app.factory("conferenceService", ['$http', '$rootScope', '$q', '$timeout', '$filter', '$route', 
+    app.factory("conferenceService", ['$http', '$rootScope', '$q', '$timeout', '$filter', '$route',
     function ($http, $rootScope, $q, $timeout, $filter, $route) {
             var meeting
             var conferenceArticle;
 
             function getActiveConference(){
 
-                if(meeting){                    
+                if(meeting){
                     return $q.when(meeting);
                 }
                 var query = {};
@@ -16,7 +16,7 @@
                 }
                 else
                     query.active = true;
-                   
+
                 return meeting = $q.when($http.get('/api/v2016/conferences', {params : { q : query, s: { StartDate: 1}}, cache:true}))
                                     .then(function(data){
                                         meeting             = _.head(data.data);
@@ -24,7 +24,7 @@
                                             meeting.conference.webcast = _.find(meeting.conference.eventLinks, {type:'webcast'});
                                             meeting.conference.media   = _.filter(meeting.conference.eventLinks, {type:'media'});
                                             meeting.conference.information   = _.filter(meeting.conference.eventLinks, function(link){return !_.includes(['media', 'webcast'], link.type)});
-                                            
+
                                             meeting.conference.sideEventLinks           = _.filter(meeting.conference.scheduleLinks, {type:'side-events'});
                                             meeting.conference.parallelMeetingLinks     = _.filter(meeting.conference.scheduleLinks, {type:'parallel-meeting'});
 
@@ -36,22 +36,54 @@
                                             return meeting;
                                         }
                                     });
-                
+
+            }
+            function getFuture(){
+              var query = {
+                            StartDate:{ $gt: { $date: new Date()  } },
+                            institution:"CBD"
+                          }
+              return $http.get('/api/v2016/conferences', {params : { q : query, f:{ StartDate:1,MajorEventIDs:1,Title:1,Venua:1,code:1,Description:1}, s: { StartDate: 1}}, cache:true}).then(
+                function(res){
+
+                  return res.data
+                }
+              )
+            }
+            function getMeetings(ids){
+              var oidArray=[];
+
+              for (var i=0; i<ids.length; i++) {
+                oidArray.push({
+                    '$oid': ids[i]
+                });
+              }
+              var query = {
+                            _id:{$in:oidArray}
+                          }
+// /
+              return  $http.get('/api/v2016/meetings', { cache:true, params: { q : query,f : { EVT_CD:1, title:1, venueText:1, dateText:1, EVT_WEB:1, EVT_INFO_PART_URL:1, EVT_REG_NOW_YN:1, EVT_STY_CD:1 }  } })
+              .then(function(res){
+  console.log('meetings',res.data)
+                  return res.data
+                }
+              )
             }
 
-
             function getConferenceArticle(articleId){
-                
-                if(conferenceArticle){                    
+
+                if(conferenceArticle){
                     return $q.when(conferenceArticle);
                 }
                 return conferenceArticle =  $q.when($http.get('/api/v2017/articles/' + articleId, {cache:true}))
                                                 .then(function(data){
                                                     return article = data.data;
                                                 });
-                
+
             }
             return {
+                getMeetings             : getMeetings,
+                getFuture               : getFuture,
                 getActiveConference     : getActiveConference,
                 getConferenceArticle    : getConferenceArticle
             }
