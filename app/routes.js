cff4939da645de4ab0e34cbd14b624855efc1cdb
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-07-04 16:35:35 -0400

    new search route

--- app/routes.js
+++ app/routes.js
@@ -51,7 +51,7 @@ define(['app', 'jquery', 'lodash', 'text!./redirect-dialog.html','providers/exte
 
       $('base').attr('href', '/decisions/'); // allow full page reload outside of  /decisions/*
       
-      routeProvider.when('/',                              { templateUrl: 'views/decisions/search.html',    resolveController: true, reloadOnSearch : false, resolve : { user : currentUser() } } );
+      routeProvider.when('/search',                              { templateUrl: 'views/decisions/search.html',    resolveController: true, reloadOnSearch : false, resolve : { user : currentUser() } } );
       routeProvider.when('/:body/:session',                      { templateUrl: 'views/decisions/list.html',      resolveController: true, resolve : { user : currentUser() } } );
       routeProvider.when('/:body/:session/:decision',            { templateUrl: 'views/decisions/view.html',      resolveController: true, resolve : { user : currentUser() } } );
       routeProvider.when('/:body/:session/:decision/edit',       { templateUrl: 'views/decisions/edit.html',      resolveController: true, resolve : { user : securize(["Administrator","DecisionTrackingTool", "ScbdStaff"]) } } );
