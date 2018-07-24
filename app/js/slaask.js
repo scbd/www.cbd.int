f102b01c0a478d0a2dc0ba9ada57a9ee3e9104c5
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-07-24 15:02:01 -0400

    load slaask from requirejs

---
+++ app/js/slaask.js
@@ -0,0 +1,25 @@
+﻿define(['require', 'https://cdn.slaask.com/chat.js'], function(require, platform) {
+
+    window.platform = platform;
+
+    window._slaask = new window.slaaskApp();
+
+    window._slaask.createScriptTag = function (url) {
+
+        var app = this;    
+        var virtualStriptTag = {};
+
+        require([url], function(emoji) {
+
+            if(!app.emoji)
+                app.emoji = emoji;
+
+            if(virtualStriptTag.onload)
+                virtualStriptTag.onload();
+        });
+
+        return virtualStriptTag;
+    };
+
+    return window._slaask;
+});
