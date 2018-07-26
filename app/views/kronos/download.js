93e1b60612e2f95f6465f25cfb640f9b92ac8887
Randy Houlahan <randy.houlahan@cbd.int>
2018-07-26 10:51:45 -0400

    wrong url

--- app/views/kronos/download.js
+++ app/views/kronos/download.js
@@ -4,12 +4,14 @@ define(['app', 'services/conference-service'], function(app) {
 
 		var _ctrl 		= this;
     _ctrl.host=window.location.origin
-    _ctrl.signed = '/api/v2018/kronos/participation-requests/'+encodeURIComponent($location.search().q)+'/sign'
 
 
 
-$http.post(_ctrl.signed).then(function(u){
+
+$http.post('/api/v2018/kronos/participation-requests/'+encodeURIComponent($location.search().q)+'/sign').then(function(u){
+  _ctrl.signed = u.data.signedUrl
   window.location=u.data.signedUrl
+
   // window.close();
 })
 
