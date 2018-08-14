64678942cb1e2667117c441799358e7c25ff6334
Randy Houlahan <randy.houlahan@cbd.int>
2018-08-14 14:30:52 -0400

    translation

--- app/directives/kronos/user-messages.js
+++ app/directives/kronos/user-messages.js
@@ -11,14 +11,7 @@ define(['app', 'text!./user-messages.html'], function(app, html) { 'use strict';
       },
 			link: function ($scope) {
         $scope.remove = remove
-        var messages = [
-          {title:'Saved', description:'Your data was successfully saved.', state:'success'},
-          {code:"STEP_NOT_COMPLETE", title:'Step incomplete', description:'You must complete the steps sequentially.', state:'danger'},
-          {title:'Manditory', description:'', state:'warning'},
-          {code:"NO_SERVICE", title:'Service not available', description:'Service is not avialibale at {{config.method}} {{config.url}}', state:'danger'},
-          {code:"NOT_FOUND", title:'Service not available or document was not found', description:'Service is not avialibale or not found  at {{config.url}}', state:'danger'},
-          {code:"INVALID_SCHEMA", title:'The data passed to the server is not in the required format.', description:'Please contact the SCBD IT unit to report this error.  it@cbd.int <pre>{{data |json}}</pre>', state:'danger'}
-        ]
+
 
 
         $scope.$watch('error',function(){
@@ -41,35 +34,13 @@ define(['app', 'text!./user-messages.html'], function(app, html) { 'use strict';
             $scope.error=false
         })
 
-        function remove(i){
-          $scope.msg.splice(i,1)
-        }
-
-        function getTemplate(code){
-          for(var i=0; i<messages.length;i++)
-            if(messages[i].code===code)return messages[i]
-
+        function remove(code){
+          delete($scope.msg[code])
         }
 
         function createMsg(code, e){
-          var msgTemplate = JSON.parse(JSON.stringify(getTemplate(code)))
-
-          for (var p in msgTemplate){
-            var intFcn = $interpolate(msgTemplate[p])
-            msgTemplate[p] = intFcn(e)
-          }
-
-          if(!Array.isArray($scope.msg))$scope.msg=[]
-
-          var exists = false
-          for (var i = 0; i < $scope.msg.length; i++)
-            if(msgTemplate.code===$scope.msg[i].code)
-              exists = true
-
-          if(!exists)
-            $scope.msg.push(msgTemplate)
-
-
+          if(!$scope.msg)$scope.msg={}
+          $scope.msg[code]={error:e}
         }
 
 			}
