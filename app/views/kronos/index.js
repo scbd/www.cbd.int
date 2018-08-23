f8c5a7fbb727608db0bb2a2f930b7cdb1c7b24be
Stephane Bilodeau <bilodeaustephane@gmail.com>
2018-08-23 14:01:36 -0400

    Kronos landing page

--- app/views/kronos/index.js
+++ app/views/kronos/index.js
@@ -3,9 +3,8 @@ define(['lodash', 'app'], function(_) {
 	return ['user', function(user) {
         
         var _ctrl = this;
-
-        _ctrl.isAuthenticated = user.isAuthenticated;
-        _ctrl.isKronosUser    = !!_.intersection(user.roles, ['Kronos-FullAccess', 'Kronos-ReadOnly', 'Kronos-Meeting-Nominator', 'Kronos-PriorityPassOnly', 'Kronos-User']).length;
         
+        _ctrl.isAuthenticated = user.isAuthenticated;
+        _ctrl.isKronosUser    = user.institution=='CBD' && !!_.intersection(user.roles, ['Kronos-FullAccess' /*'Kronos-User', 'Kronos-ReadOnly', 'Kronos-Meeting-Nominator', 'Kronos-PriorityPassOnly'*/]).length;
     }];
 });
