51d444910d64e6b937d461e91f6de5c54e034e25
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-09-11 12:02:59 -0400

    create article directive

---
+++ app/directives/articles/cbd-article.js
@@ -0,0 +1,50 @@
+define(['app', 'text!./cbd-article.html','lodash', 'services/article-service'], function(app, template, _) {
+	 'use strict';
+
+	app.directive('cbdArticle', ['$sce', '$q', 'articleService', function ($sce, $q, articleService)
+	{
+		return {
+			restrict: 'E',
+			template : template,
+			replace: true,
+			scope: {
+				query : '=query'
+			},
+			link: function ($scope, $element, $attr)
+			{
+				$scope.hideCoverImage = $attr.hideCoverImage||false;
+
+				$scope.trustedHtml = function (plainText) {
+					return $sce.trustAsHtml(plainText);
+				}
+				
+				$scope.getSizedImage = function(url){
+					if(!url)
+						return url;
+					var size = '1200x600'
+					return url.replace(/attachments\.cbd\.int\//, '$&'+size+'/')
+				}
+	
+				function loadArticle(){
+					
+					$q.when(articleService.query({ "ag" : JSON.stringify($scope.query) }))
+					.then(function(article){
+						if(article.length ==0 )
+							$scope.article = {
+								content : { en : 'No information is available for this link'}
+							}
+						else
+							$scope.article = article[0];
+					});
+				}
+				
+				// $scope.$watch('query', function(newVal, oldVal){
+				// 	if(newVal){
+						loadArticle();
+				// 	}
+				// })
+			}
+		};
+	}]);
+});
+
