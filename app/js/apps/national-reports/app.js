653aa1f96348db108a5bc64baf2adefefa4cec5d
Randy Houlahan <randy.houlahan@cbd.int>
2018-10-10 16:56:56 -0400

    roll back

--- app/js/apps/national-reports/app.js
+++ app/js/apps/national-reports/app.js
@@ -2,11 +2,9 @@ define([
     'angular',
     'ngRoute',
     'ngCookies',
-    'ngAnimate',
-    'bootstrap',
-    'directives/ui-bootstrap-custom'], function(angular) { 'use strict';
+    'bootstrap'], function(angular) { 'use strict';
 
-    var deps = ['ngRoute', 'ngCookies', 'ngAnimate', 'ui.bootstrap'];
+    var deps = ['ngRoute', 'ngCookies'];
 
     angular.defineModules(deps);
 
