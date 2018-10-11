e719aec534e22cccc6dddec6342a0a9c2dd35594
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-10-11 10:36:27 -0400

    remove shim for slaask

--- app/js/slaask.js
+++ app/js/slaask.js
@@ -1,25 +1,3 @@
-﻿define(['require', 'https://cdn.slaask.com/chat.js'], function(require, platform) {
-
-    window.platform = platform;
-
-    window._slaask = new window.slaaskApp();
-
-    window._slaask.createScriptTag = function (url) {
-
-        var app = this;    
-        var virtualStriptTag = {};
-
-        require([url], function(emoji) {
-
-            if(!app.emoji)
-                app.emoji = emoji;
-
-            if(virtualStriptTag.onload)
-                virtualStriptTag.onload();
-        });
-
-        return virtualStriptTag;
-    };
-
+﻿define(['https://cdn.slaask.com/chat.js'], function() {
     return window._slaask;
 });
