38a371ac4b4c4c75e54ff7053dc2acb88b85343c
Stephane Bilodeau <bilodeaustephane@gmail.com>
2018-11-17 17:30:28 +0200

    Location Dialog

---
+++ app/directives/print-smart/locations.js
@@ -0,0 +1,15 @@
+define([], function(){
+
+    return [{
+        code: 'capital',
+        title: 'Capital',
+        description: 'Plenary / WG-1',
+        location: 'Building 1'
+    }, {
+        code: 'summit',
+        title: 'Summit',
+        description: 'WG-2',
+        location: 'Building 2'
+    }]
+
+})
