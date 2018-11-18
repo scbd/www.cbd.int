c51eb92fdec979acce0c9711f761968f53d39c7d
Stephane Bilodeau <bilodeaustephane@gmail.com>
2018-11-18 13:51:17 +0200

    UI fix

--- app/views/meetings/documents/documents.js
+++ app/views/meetings/documents/documents.js
@@ -65,6 +65,8 @@ define(['lodash', 'angular', 'filters/lstring', 'directives/print-smart/print-sm
                     isMontreal : /montr.al.*canada/i.test((res.data.venueText||{}).en||'')
                 });
 
+                meeting.printSmart=true;
+
                 _ctrl.noTabs  = meeting.EVT_STY_CD=='BAR';
                 _ctrl.meeting = meeting;
                 _ctrl.agenda  = meeting.agenda;
@@ -161,6 +163,7 @@ define(['lodash', 'angular', 'filters/lstring', 'directives/print-smart/print-sm
                 visible : d.status=='public' && !!(d.files||[]).length
             });
             
+            d.metadata.printable = true;
             if(d.status!='public')
                 delete d.metadata.visible;
 
