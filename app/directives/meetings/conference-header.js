cdd4bd4286cb7847f4f50f18838030b1d7cace6b
Stephane Bilodeau <bilodeaustephane@gmail.com>
2019-04-08 15:54:00 -0400

    Fix boostrap 3/4 bugs

--- app/directives/meetings/conference-header.js
+++ app/directives/meetings/conference-header.js
@@ -13,9 +13,8 @@
                 var basePath = (ng.element('base').attr('href')||'').replace(/\/+$/g, '');
                 ng.element(document).ready(function () {
 
-                    $q.when(conferenceService.getActiveConference())
-                    .then(function(meeting){
-                        $scope.meeting = meeting;
+                    $q.when(conferenceService.getActiveConference()).then(function(conference){
+                        $scope.conference = $scope.meeting = conference; //Keep meeting for tmp compatibility
                     });   
 
                 });
