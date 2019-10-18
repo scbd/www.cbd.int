dab9cdcd71d8296f019e55a37fa2756c5a054bbc
Randy Houlahan <randy.houlahan@cbd.int>
2019-10-18 12:10:38 -0400

    disclaimer

---
+++ app/views/decisions/directives/disclaimer.js
@@ -0,0 +1,37 @@
+define(['app', 'text!./disclaimer.html'], function(app, template) { 'use strict';
+
+	return app.directive('disclaimer', ['$cookies', function( $cookies) {
+        
+        var state = initCookie($cookies)
+
+		return {
+            restrict : "E",
+            template : template,
+            link: function ($scope){
+                
+                $scope.state  = state
+                $scope.toggle = toggle
+
+                function toggle(){
+                    console.log($scope.state)
+                    $scope.state.hidden = !$scope.state.hidden
+                    $cookies.putObject('decisionTrackingDisclaimer', $scope.state, [{path:'/decisions/search', secure: true, samesite:true}]);
+                }
+            }
+        }
+    }]);
+});
+
+
+
+function initCookie($cookies){
+    var cookie = $cookies.get('decisionTrackingDisclaimer')
+    if(!cookie) {
+        var state = { hidden: false }
+
+        $cookies.putObject('decisionTrackingDisclaimer', state, [{path:'/decisions/search', secure: true, samesite:true}]);
+        return state;
+    }
+    
+    return JSON.parse(cookie)
+}
--- app/views/decisions/paragraph.js
+++ app/views/decisions/paragraph.js
@@ -1,4 +1,4 @@
-define(['angular', 'lodash', 'app', 'filters/lstring', 'css!./view.css', './view-element', 'filters/moment', 'filters/lodash', './directives/notification', './directives/meeting-document', './directives/meeting', './directives/decision-reference','filters/term'], function(ng, _) { 'use strict';
+define(['angular', 'lodash', 'app', 'filters/lstring', 'css!./view.css', './view-element', 'filters/moment', 'filters/lodash', './directives/notification', './directives/meeting-document', './directives/meeting', './directives/decision-reference','filters/term','./directives/header-decisions'], function(ng, _) { 'use strict';
 
     return ['$scope', '$http', '$route', '$location', '$compile', '$anchorScroll', '$filter',
     function($scope, $http, $route, $location, $compile, $anchorScroll, $filter) {
