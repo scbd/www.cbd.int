89a80af1a907b4725c47e820574510fb7c95f8e5
Blaise <blaisefonseca@hotmail.com>
2020-07-29 13:54:00 -0400

    use stable version for molecule package

--- app/boot.js
+++ app/boot.js
@@ -55,7 +55,7 @@ require.config({
         'conferenceCal'   : cdnHost + '@scbd/conference-cal@0.1.2/dist/lib/ConferenceCal.umd.min',
         'angular-cache'   : cdnHost + 'angular-cache@4.6.0/dist/angular-cache.min',
         'PageHeaderFixed' : cdnHost + '@scbd/page-header-fixed/dist/PageHeaderFixed.umd.min',
-        'PageHeader'      : cdnHost + '@scbd/page-header/dist/PageHeader.umd.min',
+        'PageHeader'      : cdnHost + '@scbd/page-header@0.0.70/dist/PageHeader.umd.min',
         'PageFooter'      : cdnHost + '@scbd/page-footer/dist/PageFooter.umd.min',
         'nlp'             : cdnHost + 'compromise/builds/compromise.min',
     },
