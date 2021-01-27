5bdc1b9032009bdd7c1e28695a3e463f323879c0
Randy Houlahan <randy.houlahan@cbd.int>
2021-01-27 11:34:33 -0500

    with i18n

--- components/meetings/sessions.vue
+++ components/meetings/sessions.vue
@@ -1,30 +1,148 @@
-<template>
-    <div>
-        
-        <h1 class="red"> Sessions hello Randy !!!</h1>
-        <hr/>
-        <myCounter></myCounter>
-        <hr/>
+
+<template >
+  <div>
+    <h1> {{ $t('hello') }} => Sessions View </h1>
+<div id="accordion">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Collapsible Group Item #1
+        </button>
+      </h5>
     </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        <table class="table table-striped table-hover sessions">
+
+          <tbody>
+            <tr>
+              <th scope="row">1.</th>
+              <td><span class="badge label agenda NP">NP 16</span></td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2.</th>
+              <td><span class="badge label agenda CBD">CBD 17</span></td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3.</th>
+              <td><span class="badge label agenda CP">CP 16</span></td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Collapsible Group Item #2
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Collapsible Group Item #3
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
+  </div>
 </template>
+
 
 <script>
 
-import ky from "ky"
-import myCounter from "./test-click-button.vue"
+import   i18n       from './locales.js'
 
-console.log(ky);
-debugger;
 export default {
-    components: { myCounter },
-    data:  () => {
-        return {
-            count: 0
-        }
-    }
+  name: 'Sessions',
+  i18n,
+  mounted
 }
 
+function mounted(){
+
+}
+
+
+
+
 </script>
+
 <style scoped>
- .red  { color: #FF00FF;}
+.sessions > th {
+  max-width: 5%;
+}
+.label.agenda.CBD {
+    color: #fff;
+    background: #009B48;
+}
+
+.label.agenda.CBD {
+    color: #fff;
+    background: #009B48;
+}
+.label.agenda.CP {
+    color: #fff;
+    background: #A05800;
+}
+
+.label.agenda.CP {
+    color: #fff;
+    background: #A05800;
+}
+.label.agenda.NP {
+    color: #fff;
+    background: #0086B7;
+}
+.label.agenda.NP {
+    color: #fff;
+    background: #0086B7;
+}
+.label.agenda {
+    color: #fff;
+    background: #777;
+    display: inline-block;
+    min-width: 55px;
+}
+.label.agenda {
+    color: #fff;
+    background: #777;
+    display: inline-block;
+    min-width: 55px;
+}
+.badge {
+    display: inline-block;
+    padding: .25em .4em;
+    font-size: 75%;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: .25rem;
+}
 </style>
