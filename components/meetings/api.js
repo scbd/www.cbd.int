21b70cd11c7d6b0f8f3449abaebf080ecb30a909
Randy Houlahan <randy.houlahan@cbd.int>
2021-02-02 16:57:38 -0500

    save

--- components/meetings/api.js
+++ components/meetings/api.js
@@ -6,59 +6,195 @@ export const getSessions = async (conferenceId='5f626862cabfb1fd7d67eac2') => {
 }
 
 
-
+const files = {
+  "application/pdf": {
+    "ar": {
+      "_id": "5fd920f55b11d0000171ff4b",
+      "name": "excop-02-03-ar.pdf",
+      "size": 997314,
+      "type": "application/pdf",
+      "language": "ar",
+      "date": "2020-12-15T20:47:49.937Z",
+      "md5": "a8ae642bc9c0b375da4e9d54309d80d7",
+      "url": "https://www.cbd.int/doc/c/a8ae/642b/c9c0b375da4e9d54309d80d7/excop-02-03-ar.pdf",
+      "deleted": false
+    },
+    "en": {
+      "_id": "5fcfb0f75b11d0000171fead",
+      "name": "excop-02-03-en.pdf",
+      "size": 726273,
+      "type": "application/pdf",
+      "language": "en",
+      "date": "2020-12-08T16:59:35.230Z",
+      "md5": "4be12ab4297c5f2ce56318d560f0a12d",
+      "url": "https://www.cbd.int/doc/c/4be1/2ab4/297c5f2ce56318d560f0a12d/excop-02-03-en.pdf",
+      "deleted": false
+    },
+    "es": {
+      "_id": "5fd921198d15b10001fadd13",
+      "name": "excop-02-03-es.pdf",
+      "size": 564012,
+      "type": "application/pdf",
+      "language": "es",
+      "date": "2020-12-15T20:48:25.850Z",
+      "md5": "f2a23cdff659280deebc5a3af0a8e29a",
+      "url": "https://www.cbd.int/doc/c/f2a2/3cdf/f659280deebc5a3af0a8e29a/excop-02-03-es.pdf",
+      "deleted": false
+    },
+    "fr": {
+      "_id": "5fd9211a5b11d0000171ff50",
+      "name": "excop-02-03-fr.pdf",
+      "size": 737399,
+      "type": "application/pdf",
+      "language": "fr",
+      "date": "2020-12-15T20:48:26.752Z",
+      "md5": "170f859f131ed10ab68adb93405a071e",
+      "url": "https://www.cbd.int/doc/c/170f/859f/131ed10ab68adb93405a071e/excop-02-03-fr.pdf",
+      "deleted": false
+    },
+    "ru": {
+      "_id": "5fd9213d0d1f2e00010fc09f",
+      "name": "excop-02-03-ru.pdf",
+      "size": 536388,
+      "type": "application/pdf",
+      "language": "ru",
+      "date": "2020-12-15T20:49:01.702Z",
+      "md5": "520de52cf492aad4a32ba40509201437",
+      "url": "https://www.cbd.int/doc/c/520d/e52c/f492aad4a32ba40509201437/excop-02-03-ru.pdf",
+      "deleted": false
+    },
+    "zh": {
+      "_id": "5fd9215c8d15b10001fadd14",
+      "name": "excop-02-03-zh.pdf",
+      "size": 1032959,
+      "type": "application/pdf",
+      "language": "zh",
+      "date": "2020-12-15T20:49:32.651Z",
+      "md5": "dfa7febfcea9d04941ff779498cbdcee",
+      "url": "https://www.cbd.int/doc/c/dfa7/febf/cea9d04941ff779498cbdcee/excop-02-03-zh.pdf",
+      "deleted": false
+    }
+  },
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document": {
+    "ar": {
+      "_id": "5fd920b05b11d0000171ff49",
+      "name": "excop-02-03-ar.docx",
+      "size": 166274,
+      "type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      "language": "ar",
+      "date": "2020-12-15T20:46:40.146Z",
+      "md5": "21accc01db963f72ba6f9fe27ea65ef6",
+      "url": "https://www.cbd.int/doc/c/21ac/cc01/db963f72ba6f9fe27ea65ef6/excop-02-03-ar.docx",
+      "deleted": false
+    },
+    "en": {
+      "_id": "5fcfb0cd8d15b10001fadc7c",
+      "name": "excop-02-03-en.docx",
+      "size": 190385,
+      "type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      "language": "en",
+      "date": "2020-12-08T16:58:53.087Z",
+      "md5": "3b87e3adeeae143933603ce405ff2354",
+      "url": "https://www.cbd.int/doc/c/3b87/e3ad/eeae143933603ce405ff2354/excop-02-03-en.docx",
+      "deleted": false
+    },
+    "es": {
+      "_id": "5fd920bd0d1f2e00010fc096",
+      "name": "excop-02-03-es.docx",
+      "size": 139761,
+      "type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      "language": "es",
+      "date": "2020-12-15T20:46:53.206Z",
+      "md5": "50b7ffdeb68299e5453e104424882945",
+      "url": "https://www.cbd.int/doc/c/50b7/ffde/b68299e5453e104424882945/excop-02-03-es.docx",
+      "deleted": false
+    },
+    "fr": {
+      "_id": "5fd920c30d1f2e00010fc097",
+      "name": "excop-02-03-fr.docx",
+      "size": 142645,
+      "type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      "language": "fr",
+      "date": "2020-12-15T20:46:59.360Z",
+      "md5": "9ef11518d970a0ce16a361044b28cc4c",
+      "url": "https://www.cbd.int/doc/c/9ef1/1518/d970a0ce16a361044b28cc4c/excop-02-03-fr.docx",
+      "deleted": false
+    },
+    "ru": {
+      "_id": "5fd920c80d1f2e00010fc098",
+      "name": "excop-02-03-ru.docx",
+      "size": 130265,
+      "type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      "language": "ru",
+      "date": "2020-12-15T20:47:04.326Z",
+      "md5": "6643642335b792ee5384799c5ec3236e",
+      "url": "https://www.cbd.int/doc/c/6643/6423/35b792ee5384799c5ec3236e/excop-02-03-ru.docx",
+      "deleted": false
+    },
+    "zh": {
+      "_id": "5fd920ce0d1f2e00010fc099",
+      "name": "excop-02-03-zh.docx",
+      "size": 121257,
+      "type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      "language": "zh",
+      "date": "2020-12-15T20:47:10.741Z",
+      "md5": "3a207eece6d7d3dada546426976f7ece",
+      "url": "https://www.cbd.int/doc/c/3a20/7eec/e6d7d3dada546426976f7ece/excop-02-03-zh.docx",
+      "deleted": false
+    }
+  }
+}
 
 
 const sessions = [
   {
-    _id : '6011572f2bfb07816e088fd9',
-    title: { en: 'Informal sessions in preperation for ExCOP2'},
-    summary : { en: 'Montreal (Online), 16-19 November 2020' }, // unused in sketches
+    _id         : '6011572f2bfb07816e088fd9',
+    title       : { en: 'Informal sessions in preperation for ExCOP2' },
+    summary     : { en: 'Montreal (Online), 16-19 November 2020' }, // unused in sketches
     conferenceId: '5f626862cabfb1fd7d67eac2',
-    meetingIds: ['52000000cbd0495c00001796', '52000000cbd0495c00001797', '52000000cbd0495c00001798'],
-    meetingCodes: ['cbd-excop-02', 'cp-exmop-01', 'np-exmop-01'], // automatically filled 
-    group: null,//String oneof [null, "WG1", "WG2"]
-    date: '2020-11-16T00:00:00.000Z',      // Scheduled start date/time in UTC (based on meeting timezone)
-    startDate: '2020-11-16T09:00:00.000Z',  // actual start in UTC (based on meeting timezone)
+    meetingIds  : ['52000000cbd0495c00001796', '52000000cbd0495c00001797', '52000000cbd0495c00001798'],
+    meetingCodes: ['cbd-excop-02', 'cp-exmop-01', 'np-exmop-01'], // automatically filled
+    group       : null,//String oneof [null, "WG1", "WG2"]
+    date        : '2020-11-16T00:00:00.000Z', // Scheduled start date/time in UTC (based on meeting timezone)
+    startDate   : '2020-11-16T09:00:00.000Z', // actual start in UTC (based on meeting timezone)
     statements:[
-      { time: '2020-11-16T09:05:00.000Z', agendaItems: [{  item: 2 }], organization: { title: 'India', type: 'gov' }, public: true, files: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es'}]  },
-      { time: '2020-11-16T09:10:00.000Z', agendaItems: [{ meeting: 'cbd-excop-02', item: 3 }], organization: { title: 'Canada', type: 'gov' },  public: true, files: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es'}]  },
-      { time: '2020-11-16T09:15:00.000Z', agendaItems: [{ meeting: 'cbd-excop-02', item: 4 }], organization: { title: 'Every Woman Hope Centre (EWHC)', type: 'NGO' }, files: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es',  public: true }]  },
-      { time: '2020-11-16T09:25:00.000Z', agendaItems: [{ meeting: 'cbd-excop-02', item: 5 }], organization: { title: 'Ireland', type: 'gov' }, files: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es'}]  },
+      { time: '2020-11-16T09:05:00.000Z', agendaItems: [{                          item: 2 }], organization: { title: 'India'                         , type: 'gov' }, public: true, files },
+      { time: '2020-11-16T09:10:00.000Z', agendaItems: [{ meeting: 'cbd-excop-02', item: 3 }], organization: { title: 'Canada'                        , type: 'gov' }, public: true, files },
+      { time: '2020-11-16T09:15:00.000Z', agendaItems: [{ meeting: 'cbd-excop-02', item: 4 }], organization: { title: 'Every Woman Hope Centre (EWHC)', type: 'NGO' },               files },
+      { time: '2020-11-16T09:25:00.000Z', agendaItems: [{ meeting: 'cbd-excop-02', item: 5 }], organization: { title: 'Ireland'                       , type: 'gov' },               files }
     ]
   },
   {
-    _id : '601427d186b9bf000128662f',
-    title: { en: 'Informal sessions in preperation for CP ExMOP 1'},
-    summary : { en: 'Montreal (Online), 16-19 November 2020' }, // unused in sketches
+    _id         : '601427d186b9bf000128662f',
+    title       : { en: 'Informal sessions in preperation for CP ExMOP 1'},
+    summary     : { en: 'Montreal (Online), 16-19 November 2020' }, // unused in sketches
     conferenceId: '5f626862cabfb1fd7d67eac2',
-    meetingIds: ['52000000cbd0495c00001796', '52000000cbd0495c00001797', '52000000cbd0495c00001798'],
-    meetingCodes: ['cbd-excop-02', 'cp-exmop-01', 'np-exmop-01'], // automatically filled 
-    group: null,//String oneof [null, "WG1", "WG2"]
-    date: '2020-11-16T00:00:00.000Z',      // Scheduled start date/time in UTC (based on meeting timezone)
-    startDate: '2020-11-16T13:00:00.000Z',  // actual start in UTC (based on meeting timezone)
+    meetingIds  : ['52000000cbd0495c00001796', '52000000cbd0495c00001797', '52000000cbd0495c00001798'],
+    meetingCodes: ['cbd-excop-02', 'cp-exmop-01', 'np-exmop-01'], // automatically filled
+    group       : null,//String oneof [null, "WG1", "WG2"]
+    date        : '2020-11-16T00:00:00.000Z', // Scheduled start date/time in UTC (based on meeting timezone)
+    startDate   : '2020-11-16T13:00:00.000Z', // actual start in UTC (based on meeting timezone)
     statements:[
-      { time: '2020-11-16T09:05:00.000Z', agendaItems: [{  meetingName: 'XXX', item: 2 }], organization: { title: 'India', type: 'gov' }, public: true, files: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es'}]  },
-
-      { time: '2020-11-16T13:10:00.000Z', agendaItems: [{ meeting: 'cp-exmop-01', item: 3 }], organization: { title: 'Canada', type: 'gov' },  public: true, files: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es'}]  },
-      { time: '2020-11-16T13:15:00.000Z', agendaItems: [{ meeting: 'cp-exmop-01', item: 4 }], organization: { title: 'Every Woman Hope Centre (EWHC)', type: 'NGO' }, files: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es',  public: true }]  },
-      { time: '2020-11-16T14:25:00.000Z', agendaItems: [{ meeting: 'cp-exmop-01', item: 5 }, { meeting: 'cp-exmop-01', item: 6 }], organization: { title: 'Ireland', type: 'gov' }, files: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es'}]  },
+      { time: '2020-11-16T09:05:00.000Z', agendaItems: [{ meetingName: 'XXX'        , item: 2 }                                     ], organization: { title: 'India'                         , type: 'gov' }, public: true, files },
+      { time: '2020-11-16T13:10:00.000Z', agendaItems: [{ meeting    : 'cp-exmop-01', item: 3 }                                     ], organization: { title: 'Canada'                        , type: 'gov' }, public: true, files },
+      { time: '2020-11-16T13:15:00.000Z', agendaItems: [{ meeting    : 'cp-exmop-01', item: 4 }                                     ], organization: { title: 'Every Woman Hope Centre (EWHC)', type: 'NGO' },               files },
+      { time: '2020-11-16T14:25:00.000Z', agendaItems: [{ meeting    : 'cp-exmop-01', item: 5 }, { meeting: 'cp-exmop-01', item: 6 }], organization: { title: 'Ireland'                       , type: 'gov' },               files }
     ]
   },
   {
-    _id : '601427d186b9bf000158662f',
-    title: { en: 'Informal sessions in preperation for NP ExMOP 1'},
-    summary : { en: 'Montreal (Online), 16-19 November 2020' }, // unused in sketches
+    _id         : '601427d186b9bf000158662f',
+    title       : { en: 'Informal sessions in preperation for NP ExMOP 1' },
+    summary     : { en: 'Montreal (Online), 16-19 November 2020' }, // unused in sketches
     conferenceId: '5f626862cabfb1fd7d67eac2',
-    meetingIds: ['52000000cbd0495c00001796', '52000000cbd0495c00001797', '52000000cbd0495c00001798'],
-    meetingCodes: ['cbd-excop-02', 'cp-exmop-01', 'np-exmop-01'], // automatically filled 
-    group: null,//String oneof [null, "WG1", "WG2"]
-    date: '2020-11-16T00:00:00.000Z',      // Scheduled start date/time in UTC (based on meeting timezone)
-    startDate: '2020-11-16T18:00:00.000Z',  // actual start in UTC (based on meeting timezone)
-    statements:[
-      { time: '2020-11-16T18:10:00.000Z', agendaItems: [{ meeting: 'np-exmop-01', item: 2 }, { meeting: 'np-exmop-01', item: 3 }], organization: { title: 'Canada', type: 'gov' }, public: true, files: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es'}]  },
-      { time: '2020-11-16T18:15:00.000Z', agendaItems: [{ meeting: 'cnp-exmop-01', item: 4 }], organization: { title: 'Every Woman Hope Centre (EWHC)', type: 'NGO' }, public: true, iles: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es',  public: true }]  },
-      { time: '2020-11-16T19:25:00.000Z', agendaItems: [{ meeting: 'np-exmop-01', item: 5 }, { meeting: 'cp-exmop-01', item: 6 }], organization: { title: 'Ireland', type: 'gov' }, files: [ { url: 'https://www.cbd.int/doc/c/c7e8/adfa/b0ecb7506ab5935e05f9b5f5/excop-02-01-en.docx', lang:'en' }, { url: 'https://www.cbd.int/doc/c/e7ff/c4e3/4e42fc19152c82f15c47a0fd/excop-02-01-es.docx', lang:'es'}]  },
+    meetingIds  : ['52000000cbd0495c00001796', '52000000cbd0495c00001797', '52000000cbd0495c00001798'],
+    meetingCodes: ['cbd-excop-02', 'cp-exmop-01', 'np-exmop-01'], // automatically filled
+    group       : null,//String oneof [null, "WG1", "WG2"]
+    date        : '2020-11-16T00:00:00.000Z', // Scheduled start date/time in UTC (based on meeting timezone)
+    startDate   : '2020-11-16T18:00:00.000Z', // actual start in UTC (based on meeting timezone)
+    statements : [
+      { time: '2020-11-16T18:10:00.000Z', agendaItems: [{ meeting: 'np-exmop-01' , item: 2 }, { meeting: 'np-exmop-01', item: 3 }], organization: { title: 'Canada'                        , type: 'gov' }, public: true, files },
+      { time: '2020-11-16T18:15:00.000Z', agendaItems: [{ meeting: 'cnp-exmop-01', item: 4 }                                     ], organization: { title: 'Every Woman Hope Centre (EWHC)', type: 'NGO' }, public: true, files },
+      { time: '2020-11-16T19:25:00.000Z', agendaItems: [{ meeting: 'np-exmop-01' , item: 5 }, { meeting: 'cp-exmop-01', item: 6 }], organization: { title: 'Ireland'                       , type: 'gov' },               files },
     ]
   }
 ]
--- components/meetings/sessions/files-view.vue
+++ components/meetings/sessions/files-view.vue
@@ -1,27 +1,119 @@
 
 <template >
-  <section>
-    <button type="button" class="btn btn-default btn-lg dropdown-toggle">
+  <div class="position-relative">
+    <button v-on:click="toggleDropdown($event)" type="button" class="d-md-none btn btn-default btn-lg">
       <i class="fa fa-arrow-circle-down" style="font-size:1.25em"/>
     </button>
+    <ul ref="dropDown" :class="{ show: this.show }" class="dropdown-menu dropdown-menu-right"  >
 
-    
-  </section>
+      <li  class="dropdown-header">Select a file to download</li>
+
+      <li class="dropdown-item ng-scope" ng-repeat="(language,types) in byLanguages track by language" style="font-size:16px;margin:4px 0px;white-space:nowrap;">
+
+          <a role="menuitem" tabindex="-1" style="display:inline-block;width:120px;font-size:inherit;" class="language ng-binding">
+              العربية
+          </a>
+
+          <span>
+              <a ng-repeat="(type, file) in ::types track by type" class="btn btn-sm btn-danger" style="margin-right:5px;font-size:16px;" target="_blank" href="https://www.cbd.int/doc/c/a270/2272/08837a5d085d01ff4c2adb17/excop-02-l-01-ar.pdf">
+                  <i class="fa fa-file-pdf-o"></i>
+              </a>
+              <a ng-repeat="(type, file) in ::types track by type" class="btn btn-sm btn-primary" style="margin-right:5px;font-size:16px;" target="_blank" href="https://www.cbd.int/doc/c/a7e4/f7d9/03b82c86d8713a2e6f6a8150/excop-02-l-01-ar.docx">
+                  <i class="fa fa-file-word-o"></i>
+              </a>
+          </span>
+      </li>
+    </ul>
+    <div class="d-md-block document-files">
+        <div v-for="( languages, mimeType ) in files" v-bind:key="mimeType" class="d-none d-md-block" >
+            <i :class="getIconClass(mimeType)" class="fa"/>
+            <span v-for=" ({ url }, lang) in languages"  v-bind:key="lang" >
+                <a target="_blank" :href="url" >
+                    <span class="d-none d-md-inline language">
+                        {{ lang| langTextFilter }}
+                    </span>
+                </a>
+            </span>
+
+        </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
+const langMap = new Map([ ['ar','العربية'], ['en','English'], ['es','Español'], ['fr','Français'], ['ru','Русский'], ['zh','中文'] ])
+const   MIMES = {
+                  'application/pdf':                                                            { priority: 10,  btn: 'btn-danger',  iconClasses: [ 'fa-file-pdf-o',        'red'   ] },
+                  'application/vnd.openxmlformats-officedocument.wordprocessingml.document' :   { priority: 20,  btn: 'btn-primary', iconClasses: [ 'fa-file-word-o',       'blue'  ] },
+                  'application/msword':                                                         { priority: 30,  btn: 'btn-primary', iconClasses: [ 'fa-file-word-o',       'blue'  ] },
+                  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' :         { priority: 40,  btn: 'btn-success', iconClasses: [ 'fa-file-excel-o',      'green' ] },
+                  'application/vnd.ms-excel':                                                   { priority: 50,  btn: 'btn-success', iconClasses: [ 'fa-file-excel-o',      'green' ] },
+                  'application/vnd.openxmlformats-officedocument.presentationml.presentation' : { priority: 60,  btn: 'btn-warning', iconClasses: [ 'fa-file-powerpoint-o', 'orange'] },
+                  'application/vnd.ms-powerpoint':                                              { priority: 70,  btn: 'btn-warning', iconClasses: [ 'fa-file-powerpoint-o', 'orange'] },
+                  'application/zip':                                                            { priority: 80,  btn: 'btn-default', iconClasses: [ 'fa-file-archive-o'             ] },
+                  'text/html':                                                                  { priority: 80,  btn: 'btn-default', iconClasses: [ 'fa-link'                       ] },
+                  'default':                                                                    { priority:999,  btn: 'btn-default', iconClasses: [ 'fa-file-o',            'orange'] }
+                }
+
 export default {
-  name: 'FilesView',
-  props:{
-    files: { type: String, required: false }, 
-  },
-  computed:{ name, styleClass },
-  i18n
+  name    :  'FilesView',
+  props   : {
+              files: { type: Object, required: false }
+            },
+  methods : { getIconClass, toggleDropdown, outSideClick },
+  filters : { langTextFilter },
+  data
 }
+
+function data(){ return { show: false} } 
+
+function toggleDropdown(e){
+
+  this.show = !this.show;
+  e.stopPropagation()
+
+  if(this.show){
+    document.addEventListener('click', this.outSideClick)
+    document.addEventListener('touchstart', this.outSideClick)
+  } else {
+    document.removeEventListener('click', this.outSideClick)
+    document.removeEventListener('touchstart', this.outSideClick)
+  }
+}
+
+function langTextFilter(langCode){ 
+  return langMap.get(langCode) || '' 
+}
+
+function getIconClass(mimeType){
+  const { iconClasses } = MIMES[ mimeType ] || MIMES[ 'default' ]
+
+  return iconClasses
+}
+
+function outSideClick(e){
+  if(this.$refs.dropDown.contains(e.target)) return e.stopPropagation()
+  this.show = false
+  e.stopPropagation()
+}
+
+
 
 </script>
 
 <style scoped>
-
+.red      { color: red; }
+.blue     { color: blue; }
+.green    { color: green; }
+.orange   { color: orange;}
+.language { display: inline }
+.dropdown-menu{ 
+  width: 250px;
+  position: absolute; 
+  will-change: transform; 
+  top: 0px; 
+  left: 0px; 
+  transform: translate3d(-216px, 48px, 0px);
+  }
 </style>
