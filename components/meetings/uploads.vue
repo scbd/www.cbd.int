6b98a8f14e006a47c53c81600dc38aa2c4e19e7b
Stephane <bilodeaustephane@gmail.com>
2021-02-13 17:04:20 -0500

    Upload UI cosmetic

--- components/meetings/uploads.vue
+++ components/meetings/uploads.vue
@@ -1,13 +1,6 @@
 <template>
 
     <div class="modal-content-upload">
-        <!-- Button trigger modal -->
-        <a class="btn btn-link" data-toggle="modal" data-target="#uploadModal">
-            <slot>
-                <button type="button"  class="btn btn-link" data-toggle="modal" data-target="#uploadModal" @click="init()" >Upload Statement</button>
-            </slot>
-        </a>
-        
         <!-- Modal -->
         <div class="modal fade" id="uploadModal" ref="uploadModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="uploadModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
@@ -16,7 +9,7 @@
                         <h4 class="modal-title" id="uploadModalLabel">
                             <i class="fa fa-arrow-circle-up"></i> Statement Submission
                         </h4>
-                        <button type="button" class="close" aria-label="Close">
+                        <button :disabled="uploading" type="button" class="close" aria-label="Close" @click="close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
@@ -27,7 +20,7 @@
                             <div class="form-group row">
                                 <label for="fileName" class="col-sm-3 col-form-label">File</label>
                                 <div class="col-sm-9">
-                                    <input type="file" class="form-control-file" id="file" ref="file" name="file" @change="setFileUpload" required 
+                                    <input :disabled="uploading" type="file" class="form-control-file" id="file" ref="file" name="file" @change="setFileUpload" required 
                                            accept=".pdf,.doc,.docx,application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document">
                                     <div class="invalid-feedback">Please select a valid file.</div>
                                 </div>
@@ -36,7 +29,7 @@
                             <div class="form-group row">
                                 <label for="agendaItem" class="col-sm-3 col-form-label">Agenda Item</label>
                                 <div class="col-sm-9">
-                                    <select class="form-control" id="agendaItem"  v-model="selectedAgendaItem" required>
+                                    <select :disabled="uploading" class="form-control" id="agendaItem"  v-model="selectedAgendaItem" required>
                                         <optgroup v-for="{ _id: meetingId, agenda, normalizedSymbol } in meetings" :key="meetingId" :label="normalizedSymbol">
                                             <option v-for="{ item, shortTitle, title } in agenda.items" :key="item" :value="{ meetingId, item }">{{item }} - {{ shortTitle || title }} </option>
                                         </optgroup>
@@ -48,7 +41,7 @@
                             <div class="form-group row">
                                 <label for="fileLanguage" class="col-sm-3 col-form-label">Language</label>
                                 <div class="col-sm-9">
-                                    <select class="form-control" id="fileLanguage" v-model="selectedLanguage">
+                                    <select :disabled="uploading" class="form-control" id="fileLanguage" v-model="selectedLanguage">
                                         <option value="ar">العربية</option>
                                         <option value="en" selected>English</option>
                                         <option value="es">Español</option>
@@ -63,7 +56,7 @@
                                 <label for="allowPublic" class="col-sm-3 col-form-label">Public on website?</label>
                                 <div class="col-sm-9">
                                     <div class="input-group">
-                                        <select class="form-control" id="allowPublic" v-model="allowPublic" required> 
+                                        <select :disabled="uploading" class="form-control" id="allowPublic" v-model="allowPublic" required> 
                                             <option value="true" selected>Yes</option>
                                             <option value="false">No</option>
                                         </select>                                    
@@ -79,7 +72,7 @@
                                 <label for="participantIdentity" class="col-sm-3 col-form-label">Participant Identity</label>
                                 <div class="col-sm-9">
                                     <div class="input-group">
-                                        <input type="text" class="form-control" id="participantIdentity" placeholder="ABCDE-12345" v-model.trim="participantIdentity" required>
+                                        <input :disabled="uploading" type="text" class="form-control" id="participantIdentity" placeholder="ABCDE-12345" v-model.trim="participantIdentity" required>
                                         <div class="input-group-append">
                                             <span class="input-group-text" data-toggle="tooltip" data-placement="auto" title="Enter your PriorityPass code or Badge code."><i class="fa fa-question-circle"></i></span>
                                         </div>      
@@ -88,19 +81,32 @@
                                     </div>
                                     <div class="form-check">
                                         <div>
-                                            <input class="form-check-input" type="checkbox" id="rememberMe" v-model="rememberMe">
+                                            <input :disabled="uploading" class="form-check-input" type="checkbox" id="rememberMe" v-model="rememberMe">
                                             <label class="form-check-label" for="rememberMe">Remember me (I use private computer)</label>
                                         </div>
                                     </div>
                                 </div>
-                            </div>  
+                            </div> 
+
+                            <div class="alert alert-warning" role="alert" v-if="error">
+                                <span v-if="error.code === 'forbidden'">
+                                    <b>Invalid or expired badged or prority-pass.</b> <br>
+                                    Please verify the priority pass number and try again.  
+                                </span>
+                                <span v-else>{{error.message||'Unknown error'}}</span>
+                            </div> 
                            
                         </form>  
                     </div>
                 
                     <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary" @click="submitForm"><i class="fa fa-upload"></i> <span class="hidden-xs">Submit</span></button>
-                        <button type="button" class="btn btn-default" @click="close()"><i class="fa fa-power-off"></i> <span class="hidden-xs">Close</span></button>
+
+                        <span v-if="uploading">
+                            <i class="fa fa-cog fa-spin"></i> Uploading file...
+                        </span>
+
+                        <button :disabled="uploading" type="submit" class="btn btn-success" @click="submitForm"><i class="fa fa-upload"></i> <span class="hidden-xs">Submit</span></button>
+                        <button :disabled="uploading" type="button" class="btn btn-default" @click="close()"><i class="fa fa-power-off"></i> <span class="hidden-xs">Close</span></button>
                     </div>
                 </div>
             </div>
@@ -112,13 +118,14 @@
 <script>
 import $    from 'jquery';
 import i18n from './locales.js'
-import Api, { isObjectId }  from './api.js'
+import Api  from './api.js'
 
 export default {
     name: 'uploadStatement',
     i18n,
     props: { 
-        route: { type: Object, required: false }
+        route: { type: Object, required: false },
+        show : { type: Boolean, required: false }
     },
     data:  function(){
         return {
@@ -129,8 +136,9 @@ export default {
             allowPublic        : null,
             participantIdentity: '',
             rememberMe         : false,
-
-            wasValidated    : false
+            wasValidated       : false,
+            uploading          : true,
+            error              : null,
         }
     },
     created() {
@@ -145,8 +153,18 @@ export default {
 
         $('[data-toggle="tooltip"]').tooltip();
     },
+    watch: {
+        show(visible) { 
+            this.resetForm()
+            $(this.$refs.uploadModal).modal(visible ? 'show' : 'hide')
+            this.$emit("update:show", visible);
+
+            if(visible) this.init();            
+         }
+    },
 
     computed: {
+
         title() {
             const [meeting] = this.meetings;
             return ((meeting||{}).title||{}).en||'';
@@ -162,8 +180,6 @@ export default {
 
     methods: {
         async init(){
-
-            console.log('upload',this.route.params)
 
             if(this.route.params.code){
                 const conferenceCode = this.route.params.code
@@ -210,16 +226,17 @@ export default {
             }
         },
 
-        close() {
-            this.resetForm()
-            $(this.$refs.uploadModal).modal('hide')
-        },
+        open() { this.$emit("update:show", true) },
+        close(){ this.$emit("update:show", false) },
   
         setFileUpload() {
             this.file = this.$refs.file.files[0];
         },
 
         async submitForm(e){
+          try {
+            this.error = null;
+            this.uploading = true;
             this.wasValidated = true;
 
             if(!this.isFormValid)
@@ -235,44 +252,25 @@ export default {
             }
             const passCode =this.participantIdentity.replace(/-/g, "");
 
-            const slot     = await this.getSlotInfo(passCode, data)
-            const response = await this.uploadSlot(slot, this.file);
+            const slot     = await this.api.createInterventionFileSlot(passCode, data)
+            const response = await this.uploadFileToSlot(slot, this.file);
 
-            if(response.ok){
-                const commited  = await this.commitSlot(slot, passCode, data.meetingId);
+            if(!response.ok) throw Error("Error uploadinf file");
 
-                if(commited){
-                    console.log('the intervention has been commited!');
-                }
-            }
-            else{
-                throw new HTTPError(`Upload error: ${response.statusText}`);
-            }
-            
+            const intervention = await this.api.commitInterventionFileSlot(slot.uid, passCode, data.meetingId);
+
+            this.$emit("notify", `Your file "${slot.metadata.filename}" has been submitted successfully`);
+
             this.close();
+
+          } catch(err) {
+              this.error = err
+          } finally {
+            this.uploading = false;
+          }
         },
 
-        async commitSlot(slot, passCode, meetingId){
-
-
-            const url  = `api/v2021/meeting-interventions/slot/${encodeURIComponent(slot.uid)}/commit`
-
-            let headers = new Headers();
-                headers.append("Authorization", "Pass "+passCode);
-                headers.append("Content-Type", "application/json");
-            
-            const data = { meetingId:  meetingId };
-
-            let requestOptions = {
-                method  : 'PUT',
-                headers : headers,
-                body    : JSON.stringify(data)
-            };
-  
-            return  await this.api.http(url, requestOptions).json();
-        },
-
-        async uploadSlot(slot, file){
+        async uploadFileToSlot(slot, file){
 
             const headers = new Headers();
                   headers.append("Content-Type", slot.contentType);
@@ -287,25 +285,10 @@ export default {
             return await this.api.http(slot.url, requestOptions);;
         },
 
-        async getSlotInfo(pass, data){
-            const url  = "api/v2021/meeting-interventions/slot";
-
-            const headers = new Headers();
-                  headers.append("Authorization", "Pass "+pass);
-                  headers.append("Content-Type", "application/json");
-            
-            const requestOptions = {
-                  method  : 'POST',
-                  headers : headers,
-                  body    : JSON.stringify(data)
-            };
-  
-            return  await this.api.http(url, requestOptions).json();
-        },
-
         resetForm(){
-            this.wasValidated = false;
-
+            this.wasValidated        = false;
+            this.error               = null;
+            this.uploading           = false;
             this.$refs.file.value    = null;
             this.file                = null;
             this.selectedAgendaItem  = null;
