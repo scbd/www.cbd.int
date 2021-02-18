d53c6fa7ee81c049e77d0964f1b8efeaa85b60ec
Stephane <bilodeaustephane@gmail.com>
2021-02-18 11:43:31 -0500

    Adjustement

--- components/meetings/sessions/view.vue
+++ components/meetings/sessions/view.vue
@@ -1,18 +1,17 @@
-
 <template >
-  <Accordion :length="sessionsLength">
-    
-    <template v-for="({ startDate, title}, index) in sessions.filter(({interventions})=>interventions.length)"  v-slot:[`header-${index}`]="">
-      {{ header({ title, startDate })}}
-    </template>
+  <div>
+    <Accordion :length="sessions.length">
+      <template v-for="({ startDate, title}, index) in sessions"  v-slot:[`header-${index}`]="">
+        {{ header({ title, startDate })}}
+      </template>
 
-    <template v-for="({ interventions }, index) in sessions.filter(({interventions})=>interventions.length)"  v-slot:[`body-${index}`]="" >
-      <Session v-if="interventions" :interventions="interventions" v-bind:key="index">
-        <InterventionRow v-for="(intervention, index) in interventions" :index="index+1" v-bind="{intervention}" v-bind:key="intervention._id"/>
-      </Session>
-    </template>
-
-  </Accordion >
+      <template v-for="({ interventions }, index) in sessions"  v-slot:[`body-${index}`]="" >
+        <Session v-bind:key="index">
+          <InterventionRow :show-time="false" :show-date="false" v-for="(intervention, index) in interventions" :index="index+1" v-bind="{intervention}" :key="intervention._id"/>
+        </Session>
+      </template>
+    </Accordion >
+  </div>
 </template>
 
 <script>
@@ -29,22 +28,19 @@ export default {
                   route:       { type: Object, required: false },
                   tokenReader: { type: Function, required: false }
                 },
-  computed   : { sessionsLength },
   methods    : { header },
   created, data
 }
 
 function data(){
-  return { sessions: [], api : null }
-}
-
-function sessionsLength(){
-  return this.sessions.filter(({interventions})=>interventions.length).length
+  return { 
+    sessions: [], 
+  }
 }
 
 async function created(){
-  this.api      = new Api(this.tokenReader);
-  this.sessions = await this.api.getSessions(this.route.params.meeting)
+  this.api = new Api(this.tokenReader);
+  this.sessions = await this.api.getSessions(this.route.params.meeting);
 }
 
 function header({title, startDate }){
