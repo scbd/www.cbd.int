565d021a5e3efcfe1ae821346e386ed971c525cd
Randy Houlahan <randy.houlahan@cbd.int>
2021-02-18 10:34:58 -0500

    fix view for statements tab

--- components/meetings/sessions/view.vue
+++ components/meetings/sessions/view.vue
@@ -1,38 +1,41 @@
 
 <template >
   <Accordion :length="sessionsLength">
+    
     <template v-for="({ startDate, title}, index) in sessions.filter(({interventions})=>interventions.length)"  v-slot:[`header-${index}`]="">
       {{ header({ title, startDate })}}
     </template>
 
     <template v-for="({ interventions }, index) in sessions.filter(({interventions})=>interventions.length)"  v-slot:[`body-${index}`]="" >
-      <Session  :interventions="interventions" v-bind:key="index"/>
+      <Session v-if="interventions" :interventions="interventions" v-bind:key="index">
+        <InterventionRow v-for="(intervention, index) in interventions" :index="index+1" v-bind="{intervention}" v-bind:key="intervention._id"/>
+      </Session>
     </template>
+
   </Accordion >
 </template>
 
 <script>
-import Session   from './session.vue'
-import Accordion from './accordion.vue'
+import   Api               from '../api.js'
+import   InterventionRow   from './intervention-row.vue'
+import   Session           from './session.vue'
+import   Accordion         from './accordion.vue'
 import { dateTimeFilter  } from '../filters.js'
-
-import { DateTime } from 'luxon'
-import Api from '../api.js'
 
 export default {
   name       : 'SessionsView',
-  components : { Session, Accordion },
+  components : { Session, Accordion, InterventionRow },
   props      : { 
-    route:       { type: Object, required: false },
-    tokenReader: { type: Function, required: false } },
+                  route:       { type: Object, required: false },
+                  tokenReader: { type: Function, required: false }
+                },
   computed   : { sessionsLength },
   methods    : { header },
-  filters    : { dateTimeFilter },
   created, data
 }
 
 function data(){
-  return { sessions: [], _api : null }
+  return { sessions: [], api : null }
 }
 
 function sessionsLength(){
@@ -42,14 +45,6 @@ function sessionsLength(){
 async function created(){
   this.api      = new Api(this.tokenReader);
   this.sessions = await this.api.getSessions(this.route.params.meeting)
-}
-
-function timeFilter (isoDateString)  {
-  return DateTime.fromISO(isoDateString).toFormat('T')
-}
-
-function dateTimeFilter (isoDateString) {
-  return DateTime.fromISO(isoDateString).toFormat('T  - cccc, d MMMM yyyy')
 }
 
 function header({title, startDate }){
