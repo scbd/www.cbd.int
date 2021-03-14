d46b34a502fc962ad2e270e85f4457c10a659545
Stephane <bilodeaustephane@gmail.com>
2021-03-14 10:08:01 -0400

    Fix dynamic vue view wrapper

--- app/views/vue-view-wrapper.js
+++ app/views/vue-view-wrapper.js
@@ -6,6 +6,8 @@ export { default as template } from './vue-view-wrapper.html';
 
 export default ['$scope', 'apiToken', '$route', 'component', function ($scope, apiToken, $route, component) {
 
+  component = component.default || component;
+
   $scope.tokenReader = function(){ return apiToken.get()}
   $scope.route       = { params : $route.current.params }
   $scope.vueOptions  = {
