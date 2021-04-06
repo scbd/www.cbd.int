ed30b3c86d1f1bc3d9816e854615817567261822
Blaise Fonseca <blaise.fonseca@cbd.int>
2021-04-06 10:53:55 -0400

    better handle spacing

--- app/views/idb-celebrations/logo.js
+++ app/views/idb-celebrations/logo.js
@@ -1,8 +1,7 @@
 define(['app',
-'https://cdn.jsdelivr.net/gh/albertjan/jquery.textFit/jquery.textFit.js',
 'https://zachleat.github.io/BigText/dist/bigtext.js',
-    'https://cdn.cbd.int/fitty@2.3.3/dist/fitty.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js'], function(app) { 'use strict';
+'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js',
+'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js'], function(app) { 'use strict';
 
     
       
@@ -47,10 +46,10 @@ define(['app',
             zh:{
                 date                : '2021年5月22日',
                 day                 : '生物多样性日',
-                collectiveTagline    : '我们是解决方案的一部分',
-                individualTagline   : '我是解决方案的一部分',
-                hashTag             : '为了大自然',
-                colletive           : '呵护自然，你我有份',
+                collectiveTagline    : '呵护自然，',
+                individualTagline   : '呵护自然，',
+                hashTag             : '有份',
+                colletive           : '呵护自然，',
                 maxfontsize         : 60 
             },
             ar:{
@@ -63,7 +62,7 @@ define(['app',
                 maxfontsize         : 60 
             }            
         }
-        $scope.name = '';
+        $scope.name = 'CBD';
         $scope.logoType = 'individual';
         $scope.language = 'en'
         $scope.saveImage = function(generateOnly) { 
@@ -73,7 +72,7 @@ define(['app',
                     $('#newImage').empty().append(canvas);
                     canvas.toBlob(function(blob) {
                         if(!generateOnly)
-                            saveAs(blob, "logo.jpg");                        
+                            saveAs(blob, "22-May-Biodiversity-Day.jpg");                        
                     });
                 }
             });
@@ -87,49 +86,22 @@ define(['app',
                 $('.boxIncon').css('direction', 'ltr')
 
             selector = selector||'.fit'
-            // fitty(selector);
             setTimeout(function(){
-                console.log( $scope.text[$scope.language].maxfontsize)
                 $('#bigtext').bigtext();
-                // $('#bigtext').textFit({multiLine:true, reProcess:true})
-                // getSize();
-                html2canvas($("#bigtext"), {
-                    onrendered: function(canvas) {
-                        var myImg       = $('.boxIncon #logoImg');
-                        var myImgHeight     = myImg.height();
-                        var margin = '-20px'
-                        if(myImgHeight > 250){
-                            myImgHeight += 20
-                        }
-                        if(myImgHeight > 200){
-                            margin = '-15px';
-                            myImgHeight += 15                            
-                        }
-                        else if(myImgHeight < 200){
-                            margin = '-10px';
-                            myImgHeight += 15
-                        }
-                        myImgHeight = 300
-                        $(canvas).css('height', myImgHeight+'px')
-                        $(canvas).css('margin-top',margin)
-                        $('#textImage').empty().append(canvas);
-                        $scope.saveImage(true);
-                    }
-                });
-                // $('.boxIncon #logoImg').css('height', $('#bigtext').height()+'px')
+                getSize();
+                $scope.saveImage(true);
             }, 200)
         }
         setTimeout(function(){
-            $scope.fitText();
-           
+            $scope.fitText();           
         }, 200)
         
        function getSize() {
-            var myImg       = $('.boxIncon #logoImg');
-            var logoDate    = $('.boxIncon #logoDate');
-            var logoDay     = $('.boxIncon #logoDay')
-            var logoTagline = $('.boxIncon #logoTagline')
-            var logoName    = $('.boxIncon #logoName')
+            var myImg       = $('.boxGenerate #logoImg');
+            var logoDate    = $('#bigtext #logoDate');
+            var logoDay     = $('#bigtext #logoDay')
+            var logoTagline = $('#bigtext #logoTagline')
+            var logoName    = $('#bigtext #logoName')
 
             var myImgHeight       = myImg      .height(),
                 logoDateHeight    = logoDate   .height(),
@@ -137,94 +109,33 @@ define(['app',
                 logoTaglineHeight = logoTagline.height(),
                 logoNameHeight    = logoName   .height();
 
-            var myImgfontSize       = parseInt(myImg      .css("fontSize")),
-                logoDatefontSize    = parseInt(logoDate   .css("fontSize")),
-                logoDayfontSize     = parseInt(logoDay    .css("fontSize")),
-                logoTaglinefontSize = parseInt(logoTagline.css("fontSize")),
-                logoNamefontSize    = parseInt(logoName   .css("fontSize")); 
+
+
+            var myImgfontSize       = parseInt(myImg      .css("fontSize")||0),
+                logoDatefontSize    = parseInt(logoDate   .css("fontSize")||0),
+                logoDayfontSize     = parseInt(logoDay    .css("fontSize")||0),
+                logoTaglinefontSize = parseInt(logoTagline.css("fontSize")||0),
+                logoNamefontSize    = parseInt(logoName   .css("fontSize")||0); 
+
+                logoDate   .css("height", logoDatefontSize    +'px')
+                logoDay    .css("height", logoDayfontSize     +'px')
+                logoTagline.css("height", logoTaglinefontSize +'px')
+                logoName   .css("height", logoNamefontSize    +'px')
+
+
             var totalFontSize = logoDatefontSize +
                                 logoDayfontSize  +
                                 logoTaglinefontSize+
                                 logoNamefontSize
-            console.log(
-                'myImgHeight      :' + myImgHeight      ,
-                'logoDateHeight   :' + logoDateHeight   ,
-                'logoDayHeight    :' + logoDayHeight    ,
-                'logoTaglineHeight:' + logoTaglineHeight,
-                'logoNameHeight   :' + logoNameHeight    ,
-                'total height : ' + (logoDateHeight  +
-                logoDayHeight   +
-                logoTaglineHeight+
-                logoNameHeight    )
-            )
+            var equalMargin = (myImgHeight-totalFontSize)/3;
+            var margin_logoDate    = ((logoDateHeight    - logoDatefontSize   )/2)
 
-            console.log( 'font size',  
-                logoDatefontSize  , 
-                logoDayfontSize    ,
-                logoTaglinefontSize,
-                logoNamefontSize ,
-                
-                totalFontSize,
-                myImgHeight-totalFontSize)
+            if($scope.language!= 'zh')
+                logoDate   .css('margin-top', '-' + (margin_logoDate   ) + 'px')
+            logoDay    .css('margin-top', '' + (equalMargin ) + 'px')
+            logoTagline.css('margin-top', '' + (equalMargin ) + 'px')
+            logoName   .css('margin-top', '' + (equalMargin ) + 'px')
 
-            console.log(   
-                logoDateHeight    - logoDatefontSize  , 
-                logoDayHeight     - logoDayfontSize    ,
-                logoTaglineHeight - logoTaglinefontSize,
-                logoNameHeight    - logoNamefontSize 
-            )
-            console.log((logoDateHeight    - logoDatefontSize   )/2, (logoDayHeight     - logoDayfontSize    )/2)
-
-            // console.log(
-            //     ((logoDayHeight     - logoDayfontSize    )/2) + ((myImgHeight-totalFontSize)/3),
-            //     ((logoTaglineHeight - logoTaglinefontSize)/2) + ((myImgHeight-totalFontSize)/3),
-            //     ((logoNameHeight    - logoNamefontSize   )/2) + ((myImgHeight-totalFontSize)/3),
-            //     ((logoDayHeight     - logoDayfontSize    )/2) , ((myImgHeight-totalFontSize)/3),
-            //     ((logoTaglineHeight - logoTaglinefontSize)/2) , ((myImgHeight-totalFontSize)/3),
-            //     ((logoNameHeight    - logoNamefontSize   )/2) , ((myImgHeight-totalFontSize)/3)
-            // )
-            var margin_logoDate    = ((logoDateHeight    - logoDatefontSize   )/2) 
-            var margin_logoDay     = ((logoDayHeight     - logoDayfontSize    )/2) 
-            var margin_logoTagline = ((logoTaglineHeight - logoTaglinefontSize)/2) 
-            var margin_logoName    = ((logoNameHeight    - logoNamefontSize   )/2) 
-            var isPositive = false;
-            console.log('margin:', margin_logoDate,   
-            margin_logoDay    ,
-            margin_logoTagline,
-            margin_logoName   ,
-
-            margin_logoDate   +
-            margin_logoDay    +
-            margin_logoTagline+
-            margin_logoName   )
-
-            var totalMargin =  ( margin_logoDate   +
-                                margin_logoDay    +
-                                margin_logoTagline+
-                                margin_logoName   )/4
-            // if($scope.name.length>0){
-            //     if($scope.name.length==1)
-            //         margin_logoName -= 10;                
-            //     else if($scope.name.length==2)
-            //         margin_logoName -= 15;                
-            //     else if($scope.name.length==3)
-            //         margin_logoName -= 20;
-            //     else{
-            //         var namePosition = $('.boxIncon #logoName').position()
-            //         margin_logoName = (logoNameHeight-logoNamefontSize)/2
-            //         isPositive=true
-            //     }
-            // }
-            
-            // logoDate   .css('margin-top', '-' + (margin_logoDate   ) + 'px')
-            // logoDay    .css('margin-top', '-' + (margin_logoDay     + (margin_logoDate)) + 'px')
-            // logoTagline.css('margin-top', '-' + (margin_logoTagline + (margin_logoDay)) + 'px')
-            // logoName   .css('margin-top', (isPositive ? '' : '-') + (margin_logoName    + (0)) + 'px')
-
-            // logoDate   .css('margin-top', '-' + (totalMargin  ) + 'px')
-            // logoDay    .css('margin-top', '-' + (totalMargin*2) + 'px')
-            // logoTagline.css('margin-top', '-' + (totalMargin*2) + 'px')
-            // logoName   .css('margin-top', '-' + (totalMargin*2) + 'px')
 
         }
         
