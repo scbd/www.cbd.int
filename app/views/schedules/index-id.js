c73359c2d0c5a9bf782b23b547463da622d3e84c
Randy Houlahan <randy.houlahan@cbd.int>
2021-05-02 12:35:40 -0400

    edit reservation button

--- app/views/schedules/index-id.js
+++ app/views/schedules/index-id.js
@@ -15,7 +15,7 @@ export { default as template } from './index-id.html';
         sameElse: 'dddd, D MMMM YYYY'
     };
 
-export default ['$scope', '$http', '$route', '$q', 'streamId', 'conferenceService', function($scope, $http, $route, $q, defaultStreamId, conferenceService) {
+export default ['$scope', '$http', '$route', '$q', 'streamId', 'conferenceService', '$rootScope', function($scope, $http, $route, $q, defaultStreamId, conferenceService, $rootScope) {
         const _ctrl = $scope.scheduleCtrl =  this;
 
         let _streamData;
@@ -42,6 +42,7 @@ export default ['$scope', '$http', '$route', '$q', 'streamId', 'conferenceServic
 
             }).then(function(conf){
                 _ctrl.conferenceTimezone = conf.timezone;
+                _ctrl.code = conf.code
                 _ctrl.all = conf.schedule.all
 
                 options.params.datetime = _ctrl.now();
@@ -72,6 +73,8 @@ export default ['$scope', '$http', '$route', '$q', 'streamId', 'conferenceServic
                 _ctrl.event              = _streamData.eventGroup;
                 _ctrl.frames             = _streamData.frames;
 
+                const hasRole = isAdmin()
+
                 _ctrl.frames.forEach(function(f){
 
                     if(!f.reservations)
@@ -83,11 +86,19 @@ export default ['$scope', '$http', '$route', '$q', 'streamId', 'conferenceServic
                         r.room = rooms[(r.location||{}).room];
                         r.videoUrl = r.video && (r.videoUrl || r.room.videoUrl)
 
+                        if(hasRole) r.editUrl = `https://eunomia.cbd.int/schedule/${encodeURIComponent(_ctrl.code)}?day=${encodeURIComponent(r.start)}&edit=${encodeURIComponent(r._id)}`
+
                         return _.defaults(r, { open : !(types[r.type]||{}).closed });
 
                     }).sortBy(sortKey).value();
                 });
             });
+
+            function isAdmin(){
+              if(!$rootScope.user) return false
+
+              return _.intersection($rootScope.user.roles, [ 'EunoAdministrator']).length > 0;
+            }
 
             //========================================
             //
