07e6954c9dc25b45468d1162cc1ebe25c0ba7846
Randy Houlahan <randy.houlahan@cbd.int>
2021-05-02 12:37:17 -0400

    fix connect button timings, use time and not passed datetime, , hide when done

--- app/views/meetings/documents/agenda.js
+++ app/views/meetings/documents/agenda.js
@@ -520,10 +520,11 @@ export default ["$scope", "$route", "$http", '$q', '$interval', 'conferenceServi
         _ctrl.getReservationTypeName = getReservationTypeName
 
         function canConnect({ type, start }) {
+          const now            = moment.tz(new Date(), getTimezone()).toDate();
           const minutes        = minutesBefore({ type })
           const canConnectTime = moment(start).subtract(minutes, 'm').toDate();
 
-          return _ctrl.now >= canConnectTime
+          return now >= canConnectTime
         }
         _ctrl.canConnect = canConnect
 
@@ -538,10 +539,19 @@ export default ["$scope", "$route", "$http", '$q', '$interval', 'conferenceServi
         _ctrl.minutesBefore = minutesBefore
 
         function isInProgress({ start, end }) {
+          const now       = moment.tz(new Date(), getTimezone()).toDate();
           const startDate = moment.tz(start, getTimezone()).toDate();
           const endDate   = moment.tz(end, getTimezone()).toDate();
 
-          return startDate <= _ctrl.now && _ctrl.now <= endDate
+          return startDate <= now  && now  <= endDate
         }
         _ctrl.isInProgress = isInProgress
+
+        function isConnectionDone({ end }) {
+          const now       = moment.tz(new Date(), getTimezone()).toDate();
+          const endDate   = moment.tz(end, getTimezone()).add(45, 'minutes').toDate();
+
+          return now > endDate
+        }
+        _ctrl.isConnectionDone = isConnectionDone
 	}];
