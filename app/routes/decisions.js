7be260c7757cf868ecc0ffb925ce1848250c4c08
Stephane <bilodeaustephane@gmail.com>
2021-05-25 09:22:41 -0400

    POC CkEditor

--- app/routes/decisions.js
+++ app/routes/decisions.js
@@ -1,5 +1,6 @@
 import app from '~/app';
 import { securize, currentUser, mapView } from './mixin';
+import * as vueViewWrapper     from '~/views/vue-view-wrapper'
 import * as angularViewWrapper from '~/views/angular-view-wrapper'
 
 // Static views
@@ -11,6 +12,7 @@ import * as paragraphView    from '~/views/decisions/paragraph'
 
 // On-demand views
 const editDecisionView  = { component: ()=>import('~/views/decisions/edit') }
+const ckeditorSample    = { component: ()=>import('~/views/decisions/ckeditor-sample.vue') }
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
@@ -24,6 +26,7 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
     .when('/:body/:session',                      { ...mapView(decisionListView),       resolve: { user : currentUser() } } )
     .when('/:body/:session/:decision',            { ...mapView(decisionView),           resolve: { user : currentUser() } } )
     .when('/:body/:session/:decision/edit',       { ...mapView(angularViewWrapper),     resolve: { ...editDecisionView, user : securize(["Administrator","DecisionTrackingTool", "ScbdStaff"]) } } )
+    .when('/:body/:session/:decision/ckeditor',   { ...mapView(vueViewWrapper),         resolve: { ...ckeditorSample,   user : securize(["Administrator","DecisionTrackingTool", "ScbdStaff"]) } } )
     .when('/:body/:session/:decision/:paragraph', { ...mapView(paragraphView),          resolve: { user : currentUser() } })
     .otherwise({redirectTo: '/404'});
 }]);
---
+++ app/views/decisions/ckeditor-sample.vue
@@ -0,0 +1,28 @@
+<template >
+  <div>
+    <ckeditor v-model="html" :editor="editorType" :config="editorConfig"></ckeditor>
+    {{html}}
+  </div>
+
+</template>
+
+<script>
+
+import ClassicEditor from 'ckeditor5';
+import { component as ckeditor } from 'vue-ckeditor5'
+
+export default {
+  name       : 'DecisionEditTranslations',
+  components : { ckeditor },
+  data
+}
+
+function data(){
+  return { 
+    editorType: ClassicEditor,
+    editorConfig: {},
+    html: '<p>hello</p>'
+  }
+}
+
+</script>
