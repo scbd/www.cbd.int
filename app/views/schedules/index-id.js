6afcada5e3279919121047c08a204ca213fc650e
Randy Houlahan <randy.houlahan@cbd.int>
2022-06-24 20:29:40 +0300

    admin video link access (#553)
    * admin video access
    
    * Do not restrict to Interactio
    
    Co-authored-by: Stephane Bilodeau <sbilodeau@users.noreply.github.com>
    Co-authored-by: Stephane <bilodeaustephane@gmail.com>

--- app/views/schedules/index-id.js
+++ app/views/schedules/index-id.js
@@ -96,7 +96,10 @@ export default ['$scope', '$http', '$route', '$q', 'streamId', 'conferenceServic
                         r.room = rooms[(r.location||{}).room];
                         r.videoUrl = r.video && (r.videoUrl || r.room.videoUrl)
 
-                        if(hasRole) r.editUrl = `https://eunomia.cbd.int/schedule/${encodeURIComponent(_ctrl.code)}?day=${encodeURIComponent(r.start)}&edit=${encodeURIComponent(r._id)}`
+                        if(hasRole) {
+                            r.editUrl       = `https://eunomia.cbd.int/schedule/${encodeURIComponent(_ctrl.code)}?day=${encodeURIComponent(r.start)}&edit=${encodeURIComponent(r._id)}`
+                            r.adminVideoUrl = r.videoUrl || r.room.videoUrl;
+                        }
 
                         return _.defaults(r, { open : !(types[r.type]||{}).closed });
 
