c98ac50d2cb8eb7a1e8f705861d2077858b66bee
Blaise Fonseca <blaise.fonseca@cbd.int>
2022-08-22 10:12:36 -0400

    add lastmodified data abd add ck class to content

--- app/views/conferences/info-note.vue
+++ app/views/conferences/info-note.vue
@@ -7,13 +7,14 @@
         </div>
         </br>
         <div class="row"  >
-            <div class="col-sm-12" v-if="showEditButton">
-                <cbd-add-new-article :admin-tags="infoNoteAdminTags" target="_self" class="btn btn-default pull-right">
+            <div class="col-sm-12">
+                 <span class="pull-left badge badge-warning" v-if="lastModifiedOn">Updated : {{lastModifiedOn | formatDate('yyyy-LL-dd HH:mm')}}</span>
+                <cbd-add-new-article v-if="showEditButton" :admin-tags="infoNoteAdminTags" target="_self" class="btn btn-default pull-right">
                     <slot><i class="fa fa-edit"></i>  Add info note</slot>
                 </cbd-add-new-article>
             </div>
             <div class="col-sm-12">
-                <articles-accordion :query="query" ></articles-accordion>        
+                <articles-accordion :query="query" @onArticlesLoad="onArticlesLoad" ></articles-accordion>        
             </div>
         </div>
     </div>
@@ -23,6 +24,8 @@
 import articlesAccordion from '~/directives/articles/articles-accordion.vue';
 import cbdArticle     from '~/directives/articles/cbd-article.vue';
 import cbdAddNewArticle from '~/directives/articles/cbd-add-new-article.vue';
+import { format as formatDate } from '~/components/meetings/datetime';
+import { sortByOrder } from 'lodash'
 
 export default {
     components : { articlesAccordion, cbdArticle, cbdAddNewArticle },
@@ -30,7 +33,8 @@ export default {
         return{
             introAdminTags : ['conferences', 'info-note', 'introduction', encodeURIComponent(this.$route.params.code)],
             infoNoteAdminTags : ['conferences', 'info-note', 'accordion', encodeURIComponent(this.$route.params.code)],
-            showEditButton : false
+            showEditButton : false,
+            lastModifiedOn : null
         }
     },
     mounted(){
@@ -43,7 +47,7 @@ export default {
             ag.push({"$match":{ "$and" : [{"adminTags":{"$all":['conferences', 'info-note', 'accordion', this.$route.params.code] }}]}});
                         
             ag.push({$sort : {'title.en':1 }});
-            ag.push({"$project" : { title:1, content:1, 'meta.createdOn':1}});
+            ag.push({"$project" : { title:1, content:1, 'meta.modifiedOn':1}});
             ag.push({"$limit":1000});
 
             return { ag : JSON.stringify(ag) };
@@ -53,11 +57,22 @@ export default {
             ag.push({"$match":{ "$and" : [{"adminTags":{"$all":['conferences', 'introduction', 'info-note', encodeURIComponent(this.$route.params.code)] }}]}});
                         
             ag.push({$sort : {'title.en':1 }});
-            ag.push({"$project" : { title:1, content:1, 'meta.createdOn':1}});
+            ag.push({"$project" : { title:1, content:1, 'meta.modifiedOn':1}});
             ag.push({"$limit":1000});
 
             return { ag : JSON.stringify(ag) };
         }
+    },
+    methods:{
+        onArticlesLoad(articles){           
+            if(articles.length){
+               this.lastModifiedOn = sortByOrder(articles, o=>o.meta.modifiedOn, 'desc')[0].meta.modifiedOn;
+            }
+            console.log(this.$route)
+        }
+    },
+    filters: {
+        formatDate
     }
 }
 </script>
