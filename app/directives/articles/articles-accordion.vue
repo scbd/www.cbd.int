983144b30395991db2010d8410b81b345143c39a
Blaise Fonseca <blaisefonseca@hotmail.com>
2022-08-22 13:53:57 -0400

    Info note update (#558)
    * add lastmodified data abd add ck class to content
    
    * button position
    
    * text

--- app/directives/articles/articles-accordion.vue
+++ app/directives/articles/articles-accordion.vue
@@ -1,19 +1,22 @@
 
 <template>
-    <div>
+    <div class="articles-accordion">
         <div id="accordion">
             <div class="card item" v-for="article in articles" :key="article._id">
                 <div class="card-header collapsed" :id="article._id" data-toggle="collapse" :data-target="'#' + article.hashTitle"
                             aria-expanded="true" aria-controls="collapseOne">
                     <h5 class="mb-0">
                         <i class="fa fa-chevron-up pull-right"></i>
+                        <span class="pull-right badge badge-warning" v-if="article.meta.modifiedOn">Last update: {{article.meta.modifiedOn | formatDate('yyyy-LL-dd HH:mm')}}</span>
                         {{ article.title.en }}
                     </h5>
                 </div>
 
                 <div :id="article.hashTitle" class="collapse" :aria-labelledby="article._id" data-parent="#accordion">
-                    <cbd-add-new-article v-if="showEditButton" :id="article._id" target="_self" class="btn btn-default pull-right"></cbd-add-new-article>
-                    <div class="card-body" v-html="(article.content||{}).en "></div>
+                    <div class="card-body">
+                         <cbd-add-new-article v-if="showEditButton" :id="article._id" target="_self" class="btn btn-default pull-right"></cbd-add-new-article>
+                         <div v-html="(article.content||{}).en " class="ck-content"></div>
+                     </div>
                 </div>
             </div>
         </div>
@@ -25,6 +28,7 @@
 import 'css!cdn!npm/@scbd/ckeditor5-build-inline-full@22.0.0/build/ckeditor.css';
 import ArticlesApi from '../../api/articles';
 import cbdAddNewArticle from './cbd-add-new-article.vue';
+import { format as formatDate } from '~/components/meetings/datetime';
 
 export default {
     name: 'articlesAccordion',
@@ -53,23 +57,34 @@ export default {
                 e.hashTitle = e.title.en.replace(/[^a-z0-9]/gi, '-').replace(/-+/g, '-');
                 return e;
             });
+            this.$emit('onArticlesLoad', this.articles);
         }
+    },
+    filters: {
+        formatDate
     }
 }
 </script>
     
 <style>
-    .card.item{
+    .articles-accordion .card.item{
         margin-bottom: 10px;
     }
-    .card-header .fa {
+    .articles-accordion .card-header .fa {
         transition: .3s transform ease-in-out;
     }
-    .card-header.collapsed .fa {
+    .articles-accordion .card-header.collapsed .fa {
         transform: rotate(180deg);
     }
-    .card-header{
+    .articles-accordion .card-header{
         cursor: pointer;
     }
+    .articles-accordion .card-body .ck-content {
+        padding-top: 5px;
+    }
 
+    .articles-accordion .card-body .ck-content p {
+        margin-bottom: 10px!important;
+    }
+    
 </style>
