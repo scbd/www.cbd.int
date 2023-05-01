02fd63ba17d6241e39c4f5381166fa7dae713481
Rodrigo Elias <rodrigo.elias@un.org>
2023-05-01 15:32:52 -0400

    CSS Uppercase/Lowercase fix

--- app/components/references/decision-card.vue
+++ app/components/references/decision-card.vue
@@ -4,17 +4,17 @@
             <a :href="decision.url" target="_blank">
                 {{ decision.symbol || decision.code }}
                 <span v-if="decision.elements[0]">
-                    <span v-if="decision.elements[0].section">
-                        {{ decision.elements[0].section | uppercase }}
+                    <span v-if="decision.elements[0].section" class="uppercase">
+                        {{ decision.elements[0].section }}
                     </span>
                     <span v-if="decision.elements[0].paragraph">
                         para {{ decision.elements[0].paragraph }}
                     </span>
-                    <span v-if="decision.elements[0].item">
-                        {{ decision.elements[0].item | lowercase }})
+                    <span v-if="decision.elements[0].item" class="lowercase">
+                        {{ decision.elements[0].item }})
                     </span>
-                    <span v-if="decision.elements[0].subitem">
-                        {{ decision.elements[0].subitem | lowercase }})
+                    <span v-if="decision.elements[0].subitem" class="lowercase">
+                        {{ decision.elements[0].subitem }})
                     </span>
                 </span>
                 <i class="fa fa-external-link" aria-hidden="true"></i>
@@ -43,17 +43,16 @@ export default {
         },
     },
     filters: {
-        lstring,
-        uppercase(text) {
-            return (text + '').toUpperCase();
-        },
-        lowercase(text) {
-            return (text + '').toLowerCase()
-        }
+        lstring
     }
 }
 </script>
 
 <style>
-
+.uppercase {
+    text-transform: uppercase;
+}
+.lowercase {
+    text-transform: lowercase;
+}
 </style>
