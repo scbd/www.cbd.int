784b63407b09739985b123c8bf70a6bf3a62ff16
Rodrigo Elias <rodrigo.elias@un.org>
2023-06-14 09:24:55 -0400

    Rename gaKey to googleAnalyticsCode
    Using the same naming convention as ABS.

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if(!process.env.API_URL) {
 const apiUrl     =  process.env.API_URL || 'https://api.cbddev.xyz';
 const gitVersion = (process.env.COMMIT  || 'UNKNOWN').substr(0, 8);
 const siteAlert  =  process.env.SITE_ALERT || '';
-const gaKey      =  process.env.GA_KEY || 'NOT_SET';
+const googleAnalyticsCode      =  process.env.GOOGLE_ANALYTICS_CODE || 'NOT_SET';
 
 console.info(`info: www.cbd.int`);
 console.info(`info: Git version: ${gitVersion}`);
@@ -62,11 +62,11 @@ app.get('/robots.txt', (req, res) => {
 app.get('/insession',    function(req, res) { res.redirect('/conferences/2016/cop-13/documents'); });
 app.get('/insession/*',  function(req, res) { res.redirect('/conferences/2016/cop-13/documents'); });
 
-app.get('/idb/*',                  function(req, res) { res.render('template-2011', { gitVersion, cdnUrl, baseLibs, captchaV2key, captchaV3key, gaKey }); });
-app.get('/biobridge*',             function(req, res) { res.render('template-2011', { gitVersion, cdnUrl, baseLibs, captchaV2key, captchaV3key, gaKey }); });
-app.get('/aichi-targets*',         function(req, res) { res.render('template-2011', { gitVersion, cdnUrl, baseLibs, captchaV2key, captchaV3key, gaKey }); });
-app.get('/kronos/media-requests*', function(req, res) { res.render('template-2011', { gitVersion, cdnUrl, baseLibs, captchaV2key, captchaV3key, gaKey }); });
-app.get('/participation*',         function(req, res) { res.render('template-2011', { gitVersion, cdnUrl, baseLibs, captchaV2key, captchaV3key, gaKey }); });
+app.get('/idb/*',                  function(req, res) { res.render('template-2011', { gitVersion, cdnUrl, baseLibs, captchaV2key, captchaV3key, googleAnalyticsCode }); });
+app.get('/biobridge*',             function(req, res) { res.render('template-2011', { gitVersion, cdnUrl, baseLibs, captchaV2key, captchaV3key, googleAnalyticsCode }); });
+app.get('/aichi-targets*',         function(req, res) { res.render('template-2011', { gitVersion, cdnUrl, baseLibs, captchaV2key, captchaV3key, googleAnalyticsCode }); });
+app.get('/kronos/media-requests*', function(req, res) { res.render('template-2011', { gitVersion, cdnUrl, baseLibs, captchaV2key, captchaV3key, googleAnalyticsCode }); });
+app.get('/participation*',         function(req, res) { res.render('template-2011', { gitVersion, cdnUrl, baseLibs, captchaV2key, captchaV3key, googleAnalyticsCode }); });
 
 app.use(require('./libs/prerender')); // set env PRERENDER_SERVICE_URL
 
