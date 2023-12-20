2c904e8b643985d308782f10e7eb60551aa78ecf
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2023-12-20 17:46:58 -0500

    Use notification body for displaying ntf if web version does not exists

--- app/util/html.js
+++ app/util/html.js
@@ -11,7 +11,6 @@ export function textToHtml(text, { preserveNewLine, preserveLink } = { preserveN
   if (preserveNewLine) html = html.replace('\n', '<br>');
   if (preserveLink)    html = html.replace(/[a-z]+?:\/\/\S+?(?=((\s|$)|\.+(\s|$)))/g, '<a target="_blank" href="$&">$&</a>');// make url clickable
   if (preserveLink)    html = html.replace(/\w+@\w+(\.\w+)+/g, '<a target="_blank" href="mailto:$&">$&</a>');// make url clickable
-  console.log(html);
 
   return html;
 }
--- app/views/notifications/index-id.js
+++ app/views/notifications/index-id.js
@@ -8,22 +8,12 @@ import '~/filters/term'
 import ArticlesApi from '~/api/articles'
 import jumpTo from '~/services/jump-to-anchor'
 import { textToHtml } from '~/util/html'
+import MIMES from '~/data/file-types'
+import { getLanguageName } from '~/data/languages';
 
 export { default as template } from './index-id.html'
 
-    var MIMES = {
-        'application/pdf':                                                            { priority: 10,  color: 'red',    btn: 'btn-danger',  icon: 'fa-file-pdf-o'   },
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document' :   { priority: 20,  color: 'blue',   btn: 'btn-primary', icon: 'fa-file-word-o'  },
-        'application/msword':                                                         { priority: 30,  color: 'blue',   btn: 'btn-primary', icon: 'fa-file-word-o'  },
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' :         { priority: 40,  color: 'green',  btn: 'btn-success', icon: 'fa-file-excel-o' },
-        'application/vnd.ms-excel':                                                   { priority: 50,  color: 'green',  btn: 'btn-success', icon: 'fa-file-excel-o' },
-        'application/vnd.openxmlformats-officedocument.presentationml.presentation' : { priority: 60,  color: 'orange', btn: 'btn-warning', icon: 'fa-file-powerpoint-o' },
-        'application/vnd.ms-powerpoint':                                              { priority: 70,  color: 'orange', btn: 'btn-warning', icon: 'fa-file-powerpoint-o' },
-        'application/zip':                                                            { priority: 80,  color: '',       btn: 'btn-default', icon: 'fa-file-archive-o' },
-        'text/html':                                                                  { priority: 80,  color: '',       btn: 'btn-default', icon: 'fa-link' },
-        'default':                                                                    { priority:999,  color: 'orange', btn: 'btn-default', icon: 'fa-file-o' }
-    };
-    export default ['$scope', '$route','$http', '$q','$sce', 'articleService', function ($scope, $route, $http, $q, $sce, articleService) {
+export default ['$scope', '$route','$http', '$q','$sce', 'articleService', function ($scope, $route, $http, $q, $sce, articleService) {
 
         var code   = $route.current.params.symbol;
         var _ctrl  = $scope.notifCtrl = this;
@@ -31,6 +21,7 @@ export { default as template } from './index-id.html'
         _ctrl.jumpTo = jumpTo;
 
         $scope.MIMES = MIMES;
+        $scope.getLanguageName = getLanguageName;
 
         loadCountries();
         loadNotification(code);
