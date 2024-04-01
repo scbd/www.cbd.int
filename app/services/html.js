568d0bcfe91beeec180b50d8e086e8430da50d80
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2024-04-01 08:45:12 -0400

    Use DOMPurify to prevent potential XSS instead of custom sanitizer

---
+++ app/services/html.js
@@ -0,0 +1,10 @@
+import DOMPurify from 'dompurify';
+
+export function encodeHtml(html) {
+    var div = document.createElement('div');
+    return div.text(html).html();
+}
+
+export function sanitizeHtml(unsafeHtml) {
+    return DOMPurify.sanitize(unsafeHtml); // only there to prevent XSS not to enforce html styling
+};
