30f9a72bfd96c1f49403ffdf08d6ef770a5b8f70
Rodrigo Elias <rodrigo.elias@cbd.int>
2024-05-06 09:59:45 -0400

    removed unecessary libs

--- app/views/decisions/decision-search.vue
+++ app/views/decisions/decision-search.vue
@@ -217,7 +217,6 @@
 
 <script>
 import _ from 'lodash';
-import axios from 'axios'
 import lstring from '~/filters/lstring.js';
 import SolrApi from '../../api/solr.js';
 import ThesaurusApi from '../../api/thesaurus.js';
