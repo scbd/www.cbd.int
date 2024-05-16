1714126b8628339d55b1c0575f5abab5cd56c262
Stephane Bilodeau <stephane.bilodeau@cbd.int>
2024-05-16 12:57:46 +0000

    Automatic sorting for documents by type/nature/symbol (#641)
    * Automatice sorting of meeting documents base on type/nature/symcol
    
    * extract lstring as pure  service to be usable by server middleware

--- app/services/meetings.js
+++ app/services/meetings.js
@@ -1,3 +1,5 @@
+import lstring from '../services/lstring.js';
+
 //==============================
 //
 //==============================
@@ -35,4 +37,77 @@ export function normalizeAgenda(agenda) {
 
 export function normalizeDocumentSymbol(symbol) {
     return symbol.toUpperCase().replace(/[^A-Z0-9\/\-\*]/gi, '').replace(/\/$/g, '');
+}
+
+const Priorities_Type = {
+    'official'       : 1,
+    'information'    : 2,
+    'reference'      : 3,
+    'in-session'     : 4,
+    'draft-decision' : 5,
+    'other'          : 6,
+}
+
+const Priorities_Natures = {
+    'limited'   : 1,
+    'crp'       : 2,
+    'non-paper' : 3,
+}
+
+//====================================
+//
+//====================================
+export function documentSortKey(d, { baseSymbol, locale } ) {
+
+    const parts = [];
+
+    parts.push(`SUPERSEDED`);
+
+    if((d.metadata||{}).superseded) parts.push(1);
+    else                            parts.push(0);
+
+    parts.push(`TYPE`);
+    parts.push(Priorities_Type[d.type] || pad('', 9)); 
+
+    parts.push(`NATURE`);
+    parts.push(Priorities_Natures[d.nature] || pad('', 9)); 
+    
+    let normalizedBaseSymbol = normalizeDocumentSymbol(baseSymbol||'');
+    let normalizedSymbol     = normalizeDocumentSymbol(d.symbol||'');
+
+    parts.push(`MATCH_BASE_SYMBOL`);
+    if(normalizedSymbol && normalizedSymbol.startsWith(normalizedBaseSymbol)) 
+        parts.push(0);
+    else if(normalizedSymbol) 
+        parts.push(1);
+    else
+        parts.push(2);
+
+    normalizedSymbol = normalizedSymbol.replace(/\d+/g, (t)=>pad(t));
+    normalizedSymbol = normalizedSymbol.replace(/\b(REV)(\d+)\b/, 'A-$1.$2')
+    normalizedSymbol = normalizedSymbol.replace(/\b(ADD)(\d+)\b/, 'B-$1.$2')
+    normalizedSymbol = normalizedSymbol.replace(/\b(L)(\d+)\b/,   'C-$1.$2')
+    normalizedSymbol = normalizedSymbol.replace(/\b(CRP)(\d+)\b/, 'D-$1.$2')
+
+    parts.push(`SYMBOL`);
+    parts.push(normalizedSymbol || pad('', 'Z'));
+    
+    parts.push(`TITLE`);
+    parts.push(lstring(d.title, locale).replace(/\s/g, '').toUpperCase());
+
+    parts.push(`ID`);
+    parts.push(d._id.toUpperCase());
+
+    const key = parts.join('-').replace(/\d+/g, (t)=>pad(t));;
+
+    // d.title.en = key;
+
+    return key;
+}
+
+//====================================
+//
+//====================================
+function pad(t, c='0') {
+    return (t||'').toString().padStart(8, c);
 }
