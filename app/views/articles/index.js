0cd0ee1a4e94b8813df1934a69c2d5c15d3bccd5
Blaise Fonseca <blaise.fonseca@un.org>
2024-07-16 13:56:46 -0400

    ICT-102 - Accordion all pages (#653)
    * Add new button and other props on accordion component
    
    * Option to add accordion module to all pages

--- app/views/articles/index.js
+++ app/views/articles/index.js
@@ -1,6 +1,7 @@
 ﻿import '~/app'
 import '~/directives/social-media'
 import CbdArticle from '~/directives/articles/cbd-article.vue';
+import ArticlesAccordion from '~/directives/articles/articles-accordion.vue';
 import Vue from 'Vue';
 import _ from 'lodash'
 import { cssEscape } from '~/util/css.escape';
@@ -9,7 +10,9 @@ export { default as template } from './index.html'
 
 export default ['$scope', '$route', function ($scope,  $route) {
        
-            Vue.component('CbdArticle', CbdArticle)
+            Vue.component('CbdArticle', CbdArticle);
+            Vue.component('ArticlesAccordion', ArticlesAccordion)
+            
 			var _ctrl = this;
 
             function buildQuery(){
@@ -32,7 +35,7 @@ export default ['$scope', '$route', function ($scope,  $route) {
                     match = {_id: { $oid: $route.current.params.articleId}}
 
                 ag.push({"$match"   : match });
-                ag.push({"$project" : { title:1, content:1, coverImage:1}});
+                ag.push({"$project" : { title:1, content:1, coverImage:1, customProperties:1}});
                 ag.push({"$sort"    : { "meta.updatedOn":-1}});
                 ag.push({"$limit"   : 1 });
 
@@ -41,14 +44,48 @@ export default ['$scope', '$route', function ($scope,  $route) {
             $scope.onArticleLoad = function(article){
 
                 $scope.article = article;
+
+                if(article.customProperties?.accordionTags){
+                    $scope.showAccordion = true
+                    $scope.accordionQuery = buildAccordionQuery()
+                }
+
                 $scope.isLoading = false;
             }
             $scope.cssEscape = function(url){
                 return cssEscape(url)
             }
 
+            $scope.onArticlesLoad = function(){
+
+            }
+
             function kebabCase(val){
                 return val.toLowerCase().replace(/\s/g, '-')
+            }
+
+            function buildAccordionQuery(){
+                if(!$scope.showAccordion)
+                    return;
+
+                $scope.accordionTags = $scope.article.customProperties.accordionTags.split(',').map(e=>e.trim());
+
+                if(!$scope.accordionTags?.length)
+                    return;
+
+                const ag = [];
+                ag.push({"$match":{ "$and" : [{"adminTags":{"$all":$scope.accordionTags }}]}});
+                  
+                // if(this.sortBy == 'modifiedOn')            
+                //     ag.push({$sort : {'meta.modifiedOn':-1 }});
+                // else 
+                //     ag.push({$sort : {'title.en':1 }}); 
+                    
+                ag.push({"$project" : { title:1, content:1, 'meta.modifiedOn':1}});
+                ag.push({"$limit":1000});
+    
+                    
+                return { ag : JSON.stringify(ag) };
             }
 
             buildQuery();
