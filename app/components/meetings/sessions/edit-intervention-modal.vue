1446c9bc2dec31a062ade1a348b623342ae74706
Rodrigo Elias <rodrigo.elias@cbd.int>
2024-09-16 16:04:43 -0400

    fixing date format

--- app/components/meetings/sessions/edit-intervention-modal.vue
+++ app/components/meetings/sessions/edit-intervention-modal.vue
@@ -115,7 +115,7 @@
                                         :href="`https://cbd.kronos-events.net/organizations/000000000000000000000000/contacts/${this.meta.createdBy.id}`"
                                         target="_blank">{{ this.meta.createdBy.name }}</a>
                                     <span v-else>{{ this.meta.createdBy.name }}</span> 
-                                    on {{ this.meta.createdOn }}
+                                    on {{ formatDate(this.meta.createdOn, 'yyyy-LL-dd HH:mm:ss') }}
                                 </div>
 
                                 <label for="UpdatedBy" class="col-sm-3 col-form-label">Updated by </label>
@@ -125,7 +125,7 @@
                                         :href="`https://cbd.kronos-events.net/organizations/000000000000000000000000/contacts/${this.meta.updatedBy.id}`"
                                         target="_blank">{{ this.meta.updatedBy.name }}</a>
                                     <span v-else>{{ this.meta.updatedBy.name }}</span> 
-                                    on {{ this.meta.updatedOn }}
+                                    on {{ formatDate(this.meta.updatedOn, 'yyyy-LL-dd HH:mm:ss') }}
                                 </div>
                             </div> 
 
@@ -156,6 +156,7 @@ import $    from 'jquery';
 import Api  from '../api.js'
 import OrganizationSearch from './organization-search.vue'
 import DateTimeSelector   from './datetime-selector.vue'
+import { format as formatDate } from '../datetime.js'
 
 export default {
     name: 'uploadStatement',
@@ -188,7 +189,7 @@ export default {
         }
     },
     computed: { canUpdateStatus, canPublish },
-    methods: { open, close, clearError, save, onOrganizationChange, isUserKronos},
+    methods: { open, close, clearError, save, onOrganizationChange, isUserKronos, formatDate },
     created,
     mounted, 
 }
