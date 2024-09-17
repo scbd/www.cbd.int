63587afa45d735d78d4a2e66674aabfd827b3591
Rodrigo Elias <rodrigo.elias@cbd.int>
2024-09-17 09:33:52 -0400

    change layout structure

--- app/components/meetings/sessions/edit-intervention-modal.vue
+++ app/components/meetings/sessions/edit-intervention-modal.vue
@@ -105,31 +105,6 @@
                                 </div>
                             </div>
 
-                            <hr>
-
-                            <div :v-if="meta" class="form-group row">
-                                <label for="CreatedBy" class="col-sm-3 col-form-label">Created by </label>
-                                <div class="col-sm-9">
-                                    <a 
-                                        v-if="isUserKronos(this.meta.createdBy.id)"
-                                        :href="`https://cbd.kronos-events.net/organizations/000000000000000000000000/contacts/${this.meta.createdBy.id}`"
-                                        target="_blank">{{ this.meta.createdBy.name }}</a>
-                                    <span v-else>{{ this.meta.createdBy.name }}</span> 
-                                    on {{ formatDate(this.meta.createdOn, 'yyyy-LL-dd HH:mm:ss') }}
-                                </div>
-
-                                <label for="UpdatedBy" class="col-sm-3 col-form-label">Updated by </label>
-                                <div class="col-sm-9">
-                                    <a 
-                                        v-if="isUserKronos(this.meta.updatedBy.id)"
-                                        :href="`https://cbd.kronos-events.net/organizations/000000000000000000000000/contacts/${this.meta.updatedBy.id}`"
-                                        target="_blank">{{ this.meta.updatedBy.name }}</a>
-                                    <span v-else>{{ this.meta.updatedBy.name }}</span> 
-                                    on {{ formatDate(this.meta.updatedOn, 'yyyy-LL-dd HH:mm:ss') }}
-                                </div>
-                            </div> 
-
-
                             <div class="alert alert-warning" role="alert" v-if="error">
                                 <span>{{error.message||'Unknown error'}}</span>
                             </div> 
@@ -138,6 +113,25 @@
                     </div>
                 
                     <div class="modal-footer">
+                        <div :v-if="meta" class="w-100">
+                            <small>
+                                Created by 
+                                <a 
+                                    v-if="isUserKronos(this.meta.createdBy.id)"
+                                    :href="`https://cbd.kronos-events.net/organizations/000000000000000000000000/contacts/${encodeURIComponent(this.meta.createdBy.id)}`"
+                                    target="_blank">{{ this.meta.createdBy.name }}</a>
+                                <span v-else>{{ this.meta.createdBy.name }}</span> 
+                                on {{ formatDate(this.meta.createdOn, 'yyyy-LL-dd HH:mm:ss') }}<br />
+
+                                Updated by
+                                <a 
+                                    v-if="isUserKronos(this.meta.updatedBy.id)"
+                                    :href="`https://cbd.kronos-events.net/organizations/000000000000000000000000/contacts/${encodeURIComponent(this.meta.updatedBy.id)}`"
+                                    target="_blank">{{ this.meta.updatedBy.name }}</a>
+                                <span v-else>{{ this.meta.updatedBy.name }}</span> 
+                                on {{ formatDate(this.meta.updatedOn, 'yyyy-LL-dd HH:mm:ss') }}
+                            </small>
+                        </div>
                         <i v-if="!!progress" class="fa fa-cog fa-spin"></i>
                         <button v-if=" canPublish" :disabled="!!progress" type="submit" class="btn btn-success" @click="save(true)"><i class="fa fa-microphone"></i> <span>Publish</span></button>
                         <button v-if="!canPublish" :disabled="!!progress" type="submit" class="btn btn-primary" @click="save()"><i class="fa fa-save"></i> <span>Save</span></button>
