00135f9131c60315b3dc0e1d3f96918a1368002c
Mathieu Desjarlais <github@desmat.ca>
2026-04-20 11:10:06 -0400

    EditorialRequestButton.parentDocumentId: string or array

--- app/components/meetings/editorial-request-button.vue
+++ app/components/meetings/editorial-request-button.vue
@@ -15,7 +15,7 @@ export default {
   name: 'EditorialRequestButton',
   props: {
     meetingId: { type: String, required: false },
-    parentDocumentId: { type: String, required: false },
+    parentDocumentId: { type: [String, Array], required: false },
     requestId: { type: String, required: false },
   },
   computed: {
@@ -25,7 +25,8 @@ export default {
       url.pathname = `/user-request/${this.requestId ?? 'new'}`;
       url.searchParams.set('returnUrl', window.location.href);
       if (this.meetingId) url.searchParams.set('meeting', this.meetingId);
-      if (this.parentDocumentId) url.searchParams.set('parentDocumentId', this.parentDocumentId);
+      if (this.parentDocumentId && typeof (this.parentDocumentId) === 'string') url.searchParams.set('parentDocumentId', this.parentDocumentId);
+      if (this.parentDocumentId && Array.isArray(this.parentDocumentId)) this.parentDocumentId.forEach((id) => url.searchParams.append('parentDocumentId', id));
 
       return url.toString();
     },
